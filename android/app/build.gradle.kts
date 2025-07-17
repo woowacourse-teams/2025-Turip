55fe5c0c32b4d840ea68f44cfab86a208bcabe7d
m6z1 <sonmyungj1zz@gmail.com>
2025-07-17 20:14:48 +0900

    feat: BuildConfig 에 baseUrl 추가

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -17,6 +19,12 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField(
+            "String",
+            "BASE_URL",
+            "\"${project.findProperty("base_url") ?: ""}\""
+        )
     }
 
     buildTypes {
