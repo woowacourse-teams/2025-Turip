6f7d33fd1b0f52a5158bcc928738141ed59a9a5c
seaniiio <cnc4750@naver.com>
2025-07-19 19:50:22 +0900

    refactor: 사용하지 않는 `@ResponseStatus` 제거

--- backend/src/main/java/turip/creator/controller/CreatorController.java
+++ backend/src/main/java/turip/creator/controller/CreatorController.java
@@ -1,13 +1,10 @@
 package turip.creator.controller;
 
-import static org.springframework.http.HttpStatus.OK;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import turip.creator.controller.dto.response.CreatorResponse;
 import turip.creator.service.CreatorService;
@@ -20,7 +17,6 @@ public class CreatorController {
     private final CreatorService creatorService;
 
     @GetMapping("/{id}")
-    @ResponseStatus(OK)
     public ResponseEntity<CreatorResponse> readCreatorById(@PathVariable Long id) {
         CreatorResponse response = creatorService.getById(id);
         return ResponseEntity.ok(response);
