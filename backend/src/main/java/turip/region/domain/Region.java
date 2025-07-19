fabdd6a9a5770bb7bc23710834ef554f92da737f
seaniiio <cnc4750@naver.com>
2025-07-19 17:45:08 +0900

    feat: Region 엔티티 생성

--- backend/src/main/java/turip/region/domain/Region.java
+++ backend/src/main/java/turip/region/domain/Region.java
@@ -1,4 +1,24 @@
 package turip.region.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@EqualsAndHashCode
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Region {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
 }
