a0620079068fd5d9f4e9fae9f1f1242691cfdc0c
seaniiio <cnc4750@naver.com>
2025-07-20 02:11:37 +0900

    feat: 지역별 컨텐츠 수 조회 기능 구현

--- backend/src/main/java/turip/content/service/ContentService.java
+++ backend/src/main/java/turip/content/service/ContentService.java
@@ -1,4 +1,16 @@
 package turip.content.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import turip.content.repository.ContentRepository;
+
+@Service
+@RequiredArgsConstructor
 public class ContentService {
+
+    private final ContentRepository contentRepository;
+
+    public int getCountByRegionName(String regionName) {
+        return contentRepository.countByRegion_Name(regionName);
+    }
 }
