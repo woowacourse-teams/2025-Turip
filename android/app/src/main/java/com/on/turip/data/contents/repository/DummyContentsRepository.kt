3eb6a704023b3f1bf312896c0aeab34df3d96912
jerry8282 <ka06196@naver.com>
2025-07-21 15:38:21 +0900

    feat: 더미 데이터 ContentsRepository 구현

---
+++ android/app/src/main/java/com/on/turip/data/contents/repository/DummyContentsRepository.kt
@@ -0,0 +1,21 @@
+package com.on.turip.data.contents.repository
+
+import com.on.turip.domain.contents.PagedContentsResult
+import com.on.turip.domain.contents.Video
+import com.on.turip.domain.contents.repository.ContentsRepository
+
+class DummyContentsRepository(
+    private val dummyData: List<Video> = Video.dummyVideos,
+) : ContentsRepository {
+    override suspend fun loadContentsNumbers(region: String): Int = dummyData.size
+
+    override suspend fun loadContents(
+        region: String,
+        size: Int,
+        lastId: Long,
+    ): PagedContentsResult =
+        PagedContentsResult(
+            videos = dummyData,
+            loadable = false,
+        )
+}
