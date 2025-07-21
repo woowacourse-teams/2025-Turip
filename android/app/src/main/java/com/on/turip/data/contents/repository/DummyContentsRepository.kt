30eeba980081aa992f0e8c5e2b78a9e3176300d7
jerry8282 <ka06196@naver.com>
2025-07-21 16:15:54 +0900

    refactor: 메서드명 변경 (loadContentsNumbers -> loadContentsSize)

--- android/app/src/main/java/com/on/turip/data/contents/repository/DummyContentsRepository.kt
+++ android/app/src/main/java/com/on/turip/data/contents/repository/DummyContentsRepository.kt
@@ -7,7 +7,7 @@ import com.on.turip.domain.contents.repository.ContentsRepository
 class DummyContentsRepository(
     private val dummyData: List<Video> = Video.dummyVideos,
 ) : ContentsRepository {
-    override suspend fun loadContentsNumbers(region: String): Int = dummyData.size
+    override suspend fun loadContentsSize(region: String): Int = dummyData.size
 
     override suspend fun loadContents(
         region: String,
