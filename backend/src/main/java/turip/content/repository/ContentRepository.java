7ada3e2b2b91e46eaad768b85e89470ee3f16e16
RaZel713 <razel7130@gmail.com>
2025-07-22 02:04:45 +0900

    feat: 지역을 기반으로 컨텐츠 조회하는 repository 메서드 작성

--- backend/src/main/java/turip/content/repository/ContentRepository.java
+++ backend/src/main/java/turip/content/repository/ContentRepository.java
@@ -1,9 +1,15 @@
 package turip.content.repository;
 
+import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import turip.content.domain.Content;
 
 public interface ContentRepository extends JpaRepository<Content, Long> {
 
     int countByRegion_Name(String regionName);
+
+    List<Content> findByRegion_NameAndIdLessThanOrderByIdDesc(String regionName, Long lastId, Pageable pageable);
+
+    List<Content> findByRegion_NameOrderByIdDesc(String regionName, Pageable pageable);
 }
