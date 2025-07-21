09e0ac8d649332d7ca839cabe61d1e81a318a587
yrsel <xxknu88@gmail.com>
2025-07-22 01:00:25 +0900

    feat: WebView Activity와 연결

--- android/app/src/main/java/com/on/turip/ui/search/detail/SearchDetailActivity.kt
+++ android/app/src/main/java/com/on/turip/ui/search/detail/SearchDetailActivity.kt
@@ -1,14 +1,20 @@
 package com.on.turip.ui.search.detail
 
 import android.content.Intent
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.view.View
 import android.webkit.WebView
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.viewModels
 import androidx.core.net.toUri
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.on.turip.databinding.ActivitySearchDetailBinding
 import com.on.turip.ui.common.base.BaseActivity
+import com.on.turip.ui.search.detail.webview.TuripWebChromeClient
+import com.on.turip.ui.search.detail.webview.TuripWebViewClient
 import com.on.turip.ui.search.detail.webview.WebViewVideoBridge
 import com.on.turip.ui.search.detail.webview.applyVideoSettings
 
@@ -18,6 +24,7 @@ class SearchDetailActivity : BaseActivity<SearchDetailViewModel, ActivitySearchD
         ActivitySearchDetailBinding.inflate(layoutInflater)
     }
     private lateinit var webView: WebView
+    private lateinit var turipWebChromeClient: TuripWebChromeClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,7 +49,13 @@ class SearchDetailActivity : BaseActivity<SearchDetailViewModel, ActivitySearchD
             this,
             object : OnBackPressedCallback(true) {
                 override fun handleOnBackPressed() {
-                    finish()
+                    if (turipWebChromeClient.isFullScreen()) {
+                        turipWebChromeClient.onHideCustomView()
+                    } else if (webView.canGoBack()) {
+                        webView.goBack()
+                    } else {
+                        finish()
+                    }
                 }
             },
         )
@@ -53,12 +66,50 @@ class SearchDetailActivity : BaseActivity<SearchDetailViewModel, ActivitySearchD
     }
 
     private fun setupWebView() {
+        turipWebChromeClient =
+            TuripWebChromeClient(
+                fullScreenView = binding.flSearchDetailVideoFullscreen,
+                onEnterFullScreen = ::enableFullscreen,
+                onExitFullScreen = ::disableFullscreen,
+            )
+
         webView.applyVideoSettings()
+        webView.webChromeClient = turipWebChromeClient
+        webView.webViewClient = TuripWebViewClient(binding.pbSearchDetailVideo)
         webView.addJavascriptInterface(
-            WebViewVideoBridge("") { showWebViewErrorView() }, // TODO 생성자에 url의 일부 추출 필요, WebViewUtils에 구현한 String.extractVideoId() 사용 예정
+            // TODO 생성자에 url의 일부 추출 필요, WebViewUtils에 구현한 String.extractVideoId() 사용 예정
+            WebViewVideoBridge("") { showWebViewErrorView() },
             BRIDGE_NAME_IN_JS_FILE,
         )
         webView.loadUrl(LOAD_URL_FILE_PATH)
+    }
+
+    private fun enableFullscreen() {
+        WindowCompat.setDecorFitsSystemWindows(this.window, false)
+        WindowInsetsControllerCompat(window, window.decorView).apply {
+            hide(WindowInsetsCompat.Type.systemBars())
+            systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+
+        binding.tbSearchDetail.visibility = View.GONE
+        binding.nsvSearchDetail.visibility = View.GONE
+
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+    }
+
+    private fun disableFullscreen() {
+        WindowCompat.setDecorFitsSystemWindows(this.window, true)
+        WindowInsetsControllerCompat(window, window.decorView).apply {
+            show(WindowInsetsCompat.Type.systemBars())
+            systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+
+        binding.tbSearchDetail.visibility = View.VISIBLE
+        binding.nsvSearchDetail.visibility = View.VISIBLE
+
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
     }
 
     private fun showWebViewErrorView() {
@@ -71,7 +122,10 @@ class SearchDetailActivity : BaseActivity<SearchDetailViewModel, ActivitySearchD
     private fun setupListeners() {
         binding.tvSearchDetailVideoError.setOnClickListener {
             val intent =
-                Intent(Intent.ACTION_VIEW, "".toUri()) // TODO "" 에는 유튜브 영상 uri가 들어가야 한다.
+                Intent(
+                    Intent.ACTION_VIEW,
+                    "".toUri(),
+                ) // TODO "" 에는 유튜브 영상 uri가 들어가야 한다.
             startActivity(intent)
         }
     }
