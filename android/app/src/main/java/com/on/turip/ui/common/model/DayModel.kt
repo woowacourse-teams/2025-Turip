ef4afe4fb815b21bea33b3b0ffae51e9e90eeb36
yrsel <xxknu88@gmail.com>
2025-07-22 16:04:18 +0900

    feat: Day UI Model 구현

---
+++ android/app/src/main/java/com/on/turip/ui/common/model/DayModel.kt
@@ -0,0 +1,14 @@
+package com.on.turip.ui.common.model
+
+data class DayModel(
+    val day: Int,
+    val isSelected: Boolean,
+)
+
+fun Int.initDayModels(): List<DayModel> = (1..this).map { it.initDayModel() }
+
+private fun Int.initDayModel(): DayModel =
+    when (this) {
+        1 -> DayModel(1, true)
+        else -> DayModel(this, false)
+    }
