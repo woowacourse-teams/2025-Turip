56d3ae3feba808a892144ebdf7d0f98e06780b37
yrsel <xxknu88@gmail.com>
2025-07-22 20:26:17 +0900

    feat: SearchDetailViewModel 장소 캐싱 및 LiveData 구현

--- android/app/src/main/java/com/on/turip/ui/search/detail/SearchDetailViewModel.kt
+++ android/app/src/main/java/com/on/turip/ui/search/detail/SearchDetailViewModel.kt
@@ -4,15 +4,24 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.on.turip.ui.common.model.DayModel
+import com.on.turip.ui.common.model.PlaceModel
+import com.on.turip.ui.common.model.getPlaceDummyWithDay
 import com.on.turip.ui.common.model.initDayModels
 
 class SearchDetailViewModel : ViewModel() {
-    private val _days: MutableLiveData<List<DayModel>> = MutableLiveData()
+    private val _days: MutableLiveData<List<DayModel>> = MutableLiveData(emptyList())
     val days: LiveData<List<DayModel>> = _days
 
+    private val _places: MutableLiveData<List<PlaceModel>> = MutableLiveData(emptyList())
+    val places: LiveData<List<PlaceModel>> = _places
+
+    private var placeCacheByDay: Map<DayModel, List<PlaceModel>>
+
     init {
-        val loadDay: Int = 10 // TODO: 서버에서 받아온 여행일정 X일을 추출해서 추가
+        val loadDay: Int = 3 // TODO: 서버에서 받아온 여행일정 X일을 추출해서 추가
         _days.value = loadDay.initDayModels()
+        placeCacheByDay = getPlaceDummyWithDay()
+        _places.value = placeCacheByDay[DayModel(1)]
     }
 
     fun selectDay(selectedDay: DayModel) {
@@ -26,5 +35,6 @@ class SearchDetailViewModel : ViewModel() {
                 }
             }
         _days.value = newList
+        _places.value = placeCacheByDay[selectedDay]
     }
 }
