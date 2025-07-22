f0f639774a9863207a508f04f1db0ea4d56316e8
yrsel <xxknu88@gmail.com>
2025-07-22 20:41:42 +0900

    refactor: 더미데이터 ktlint 허용 길이에 맞도록 수정

--- android/app/src/main/java/com/on/turip/ui/common/model/PlaceModel.kt
+++ android/app/src/main/java/com/on/turip/ui/common/model/PlaceModel.kt
@@ -16,27 +16,61 @@ fun getPlaceDummyWithDay(): Map<DayModel, List<PlaceModel>> {
                 PlaceModel(
                     "경포도립공원",
                     "관광지",
-                    "https://map.naver.com/p/entry/place/13300191?c=15.71,0,0,0,dh&placePath=/home?from=map&fromPanelNum=1&additionalHeight=76&timestamp=202507222020&locale=ko&svcName=map_pcv5",
+                    """https://map.naver.com/p/
+                        |entry/place/13300191?c=15.71,0,0,
+                        |0,dh&placePath=/home?from=map&fromPan
+                        |elNum=1&additionalHeight=76&timestamp=202
+                        |507222020&locale=ko&svcName=map_pcv5
+                    """.trimMargin(),
                 ),
                 PlaceModel(
                     "최일순 짬뽕순두부",
                     "음식점",
-                    "https://map.naver.com/p/entry/place/1161455816?c=15.71,0,0,0,dh&placePath=/home?from=map&fromPanelNum=1&additionalHeight=76&timestamp=202507222019&locale=ko&svcName=map_pcv5",
+                    """https://map.naver.com/p/ent
+                        |ry/place/1161455816?c=15.71,0,0,0,d
+                        |h&placePath=/home?from=map&fromPanelNu
+                        |m=1&additionalHeight=76&timestamp=2025072
+                        |22019&locale=ko&svcName=map_pcv5
+                    """.trimMargin(),
                 ),
                 PlaceModel(
                     "경포대 해수욕장3",
                     "관광지",
-                    "https://map.naver.com/p/search/%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5/place/12079976?c=15.00,0,0,0,dh&placePath=/home?entry=bmp&from=map&fromPanelNum=2&timestamp=202507222010&locale=ko&svcName=map_pcv5&searchText=%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5",
+                    """https://map.naver.com/p/
+                        |search/%EA%B2%BD%ED%8F%AC%EB%8C%80%
+                        |20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5/
+                        |place/12079976?c=15.00,0,0,0,dh&placePath=/ho
+                        |me?entry=bmp&from=map&fromPanelNum=2&timestamp=
+                        |202507222010&locale=ko&svcName=map_pcv5&searchText
+                        |=%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%9
+                        |5%EC%9E%A5
+                    """.trimMargin(),
                 ),
                 PlaceModel(
                     "경포대 해수욕장4",
                     "관광지",
-                    "https://map.naver.com/p/search/%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5/place/12079976?c=15.00,0,0,0,dh&placePath=/home?entry=bmp&from=map&fromPanelNum=2&timestamp=202507222010&locale=ko&svcName=map_pcv5&searchText=%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5",
+                    """https://map.naver.com/p/
+                        |search/%EA%B2%BD%ED%8F%AC%EB%8C%80%
+                        |20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5/
+                        |place/12079976?c=15.00,0,0,0,dh&placePath=/ho
+                        |me?entry=bmp&from=map&fromPanelNum=2&timestamp=
+                        |202507222010&locale=ko&svcName=map_pcv5&searchText
+                        |=%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%9
+                        |5%EC%9E%A5
+                    """.trimMargin(),
                 ),
                 PlaceModel(
                     "경포대 해수욕장5",
                     "관광지",
-                    "https://map.naver.com/p/search/%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5/place/12079976?c=15.00,0,0,0,dh&placePath=/home?entry=bmp&from=map&fromPanelNum=2&timestamp=202507222010&locale=ko&svcName=map_pcv5&searchText=%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5",
+                    """https://map.naver.com/p/
+                        |search/%EA%B2%BD%ED%8F%AC%EB%8C%80%
+                        |20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5/
+                        |place/12079976?c=15.00,0,0,0,dh&placePath=/ho
+                        |me?entry=bmp&from=map&fromPanelNum=2&timestamp=
+                        |202507222010&locale=ko&svcName=map_pcv5&searchText
+                        |=%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%9
+                        |5%EC%9E%A5
+                    """.trimMargin(),
                 ),
             ),
         dayTwo to
@@ -44,27 +78,55 @@ fun getPlaceDummyWithDay(): Map<DayModel, List<PlaceModel>> {
                 PlaceModel(
                     "2페이지 경포대 해수욕장",
                     "관광지",
-                    "https://map.naver.com/p/search/%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5/place/12079976?c=15.00,0,0,0,dh&placePath=/home?entry=bmp&from=map&fromPanelNum=2&timestamp=202507222010&locale=ko&svcName=map_pcv5&searchText=%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5",
+                    """https://map.naver.com/p/
+                        |search/%EA%B2%BD%ED%8F%AC%EB%8C%80%
+                        |20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5/
+                        |place/12079976?c=15.00,0,0,0,dh&placePath=/ho
+                        |me?entry=bmp&from=map&fromPanelNum=2&timestamp=
+                        |202507222010&locale=ko&svcName=map_pcv5&searchText
+                        |=%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%9
+                        |5%EC%9E%A5
+                    """.trimMargin(),
                 ),
                 PlaceModel(
                     "경포동해횟집",
                     "음식점",
-                    "https://map.naver.com/p/search/%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5/place/38590364?c=15.00,0,0,0,dh&placePath=/home?from=map&fromPanelNum=2&timestamp=202507222011&locale=ko&svcName=map_pcv5&searchText=%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5",
+                    """https://map.naver.com/p/search/%EA%
+                        |B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5/place/12079976?c=15.00,0,0,0,dh&placePath=/home?entry=bm
+                        |&from=map&fromPanelNum=2&timestamp=202507222010&locale=ko&svcName=map_pcv5&se
+                        |archText=%EA%B2%BD%ED%8F%AC%EB%8C%80
+                        |%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5
+                    """.trimMargin(),
                 ),
                 PlaceModel(
                     "경포대 해수욕장3",
                     "관광지",
-                    "https://map.naver.com/p/search/%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5/place/12079976?c=15.00,0,0,0,dh&placePath=/home?entry=bmp&from=map&fromPanelNum=2&timestamp=202507222010&locale=ko&svcName=map_pcv5&searchText=%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5",
+                    """https://map.naver.com/p/search/%EA%
+                        |B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5/place/12079976?c=15.00,0,0,0,dh&placePath=/home?entry=bm
+                        |&from=map&fromPanelNum=2&timestamp=202507222010&locale=ko&svcName=map_pcv5&se
+                        |archText=%EA%B2%BD%ED%8F%AC%EB%8C%80
+                        |%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5
+                    """.trimMargin(),
                 ),
                 PlaceModel(
                     "경포대 해수욕장4",
                     "관광지",
-                    "https://map.naver.com/p/search/%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5/place/12079976?c=15.00,0,0,0,dh&placePath=/home?entry=bmp&from=map&fromPanelNum=2&timestamp=202507222010&locale=ko&svcName=map_pcv5&searchText=%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5",
+                    """https://map.naver.com/p/search/%EA%
+                        |B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5/place/12079976?c=15.00,0,0,0,dh&placePath=/home?entry=bm
+                        |&from=map&fromPanelNum=2&timestamp=202507222010&locale=ko&svcName=map_pcv5&se
+                        |archText=%EA%B2%BD%ED%8F%AC%EB%8C%80
+                        |%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5
+                    """.trimMargin(),
                 ),
                 PlaceModel(
                     "경포대 해수욕장5",
                     "관광지",
-                    "https://map.naver.com/p/search/%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5/place/12079976?c=15.00,0,0,0,dh&placePath=/home?entry=bmp&from=map&fromPanelNum=2&timestamp=202507222010&locale=ko&svcName=map_pcv5&searchText=%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5",
+                    """https://map.naver.com/p/search/%EA%
+                        |B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5/place/12079976?c=15.00,0,0,0,dh&placePath=/home?entry=bm
+                        |&from=map&fromPanelNum=2&timestamp=202507222010&locale=ko&svcName=map_pcv5&se
+                        |archText=%EA%B2%BD%ED%8F%AC%EB%8C%80
+                        |%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5
+                    """.trimMargin(),
                 ),
             ),
         dayThree to
@@ -72,27 +134,52 @@ fun getPlaceDummyWithDay(): Map<DayModel, List<PlaceModel>> {
                 PlaceModel(
                     "3페이지 경포대 해수욕장",
                     "관광지",
-                    "https://map.naver.com/p/search/%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5/place/12079976?c=15.00,0,0,0,dh&placePath=/home?entry=bmp&from=map&fromPanelNum=2&timestamp=202507222010&locale=ko&svcName=map_pcv5&searchText=%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5",
+                    """https://map.naver.com/p/search/%EA%
+                        |B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5/place/12079976?c=15.00,0,0,0,dh&placePath=/home?entry=bm
+                        |&from=map&fromPanelNum=2&timestamp=202507222010&locale=ko&svcName=map_pcv5&se
+                        |archText=%EA%B2%BD%ED%8F%AC%EB%8C%80
+                        |%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5
+                    """.trimMargin(),
                 ),
                 PlaceModel(
                     "경포동해횟집",
                     "음식점",
-                    "https://map.naver.com/p/search/%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5/place/38590364?c=15.00,0,0,0,dh&placePath=/home?from=map&fromPanelNum=2&timestamp=202507222011&locale=ko&svcName=map_pcv5&searchText=%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5",
+                    """https://map.naver.com/p/search/%EA%
+                        |B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5/place/12079976?c=15.00,0,0,0,dh&placePath=/home?entry=bm
+                        |&from=map&fromPanelNum=2&timestamp=202507222010&locale=ko&svcName=map_pcv5&se
+                        |archText=%EA%B2%BD%ED%8F%AC%EB%8C%80
+                        |%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5
+                    """.trimMargin(),
                 ),
                 PlaceModel(
                     "경포대 해수욕장3",
                     "관광지",
-                    "https://map.naver.com/p/search/%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5/place/12079976?c=15.00,0,0,0,dh&placePath=/home?entry=bmp&from=map&fromPanelNum=2&timestamp=202507222010&locale=ko&svcName=map_pcv5&searchText=%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5",
+                    """https://map.naver.com/p/search/%EA%
+                        |B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5/place/12079976?c=15.00,0,0,0,dh&placePath=/home?entry=bm
+                        |&from=map&fromPanelNum=2&timestamp=202507222010&locale=ko&svcName=map_pcv5&se
+                        |archText=%EA%B2%BD%ED%8F%AC%EB%8C%80
+                        |%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5
+                    """.trimMargin(),
                 ),
                 PlaceModel(
                     "경포대 해수욕장4",
                     "관광지",
-                    "https://map.naver.com/p/search/%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5/place/12079976?c=15.00,0,0,0,dh&placePath=/home?entry=bmp&from=map&fromPanelNum=2&timestamp=202507222010&locale=ko&svcName=map_pcv5&searchText=%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5",
+                    """https://map.naver.com/p/search/%EA%
+                        |B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5/place/12079976?c=15.00,0,0,0,dh&placePath=/home?entry=bm
+                        |&from=map&fromPanelNum=2&timestamp=202507222010&locale=ko&svcName=map_pcv5&se
+                        |archText=%EA%B2%BD%ED%8F%AC%EB%8C%80
+                        |%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5
+                    """.trimMargin(),
                 ),
                 PlaceModel(
                     "경포대 해수욕장5",
                     "관광지",
-                    "https://map.naver.com/p/search/%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5/place/12079976?c=15.00,0,0,0,dh&placePath=/home?entry=bmp&from=map&fromPanelNum=2&timestamp=202507222010&locale=ko&svcName=map_pcv5&searchText=%EA%B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5",
+                    """https://map.naver.com/p/search/%EA%
+                        |B2%BD%ED%8F%AC%EB%8C%80%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5/place/12079976?c=15.00,0,0,0,dh&placePath=/home?entry=bm
+                        |&from=map&fromPanelNum=2&timestamp=202507222010&locale=ko&svcName=map_pcv5&se
+                        |archText=%EA%B2%BD%ED%8F%AC%EB%8C%80
+                        |%20%ED%95%B4%EC%88%98%EC%9A%95%EC%9E%A5
+                    """.trimMargin(),
                 ),
             ),
     )
