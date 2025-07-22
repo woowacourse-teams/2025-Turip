e2ce62efde982a6db4e45ea7af865b9c6ba5c23b
jerry8282 <ka06196@naver.com>
2025-07-22 13:18:44 +0900

    feat: SearchResultViewModel 기본 구조 구현

--- android/app/src/main/java/com/on/turip/ui/search/result/SearchResultViewModel.kt
+++ android/app/src/main/java/com/on/turip/ui/search/result/SearchResultViewModel.kt
@@ -1,5 +1,24 @@
 package com.on.turip.ui.search.result
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.on.turip.data.contents.repository.DummyContentsRepository
+import com.on.turip.domain.contents.repository.ContentsRepository
+import com.on.turip.domain.region.Region
+import com.on.turip.ui.common.model.SearchResultModel
 
-class SearchResultViewModel : ViewModel()
+class SearchResultViewModel(
+    val contentsRepository: ContentsRepository = DummyContentsRepository(),
+) : ViewModel() {
+    data class SearchResultState(
+        val loading: Boolean = true,
+        val error: Boolean = false,
+        val region: Region,
+        val searchResultCount: Int,
+        val searchResultModel: SearchResultModel,
+    )
+
+    private val _searchResultState: MutableLiveData<SearchResultState> = MutableLiveData()
+    val searchResultState: LiveData<SearchResultState> = _searchResultState
+}
