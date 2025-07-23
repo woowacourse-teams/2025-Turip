073a0260ef86232b9023b06a07878db12ae38096
jerry8282 <ka06196@naver.com>
2025-07-23 10:45:18 +0900

    feat: 검색 결과 화면 구성

--- android/app/src/main/java/com/on/turip/ui/search/result/SearchResultActivity.kt
+++ android/app/src/main/java/com/on/turip/ui/search/result/SearchResultActivity.kt
@@ -3,17 +3,20 @@ package com.on.turip.ui.search.result
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.viewModels
+import com.on.turip.R
 import com.on.turip.databinding.ActivitySearchResultBinding
 import com.on.turip.ui.common.base.BaseActivity
+import com.on.turip.ui.common.model.RegionModel
 
 class SearchResultActivity : BaseActivity<SearchResultViewModel, ActivitySearchResultBinding>() {
     override val viewModel: SearchResultViewModel by viewModels()
     override val binding: ActivitySearchResultBinding by lazy {
         ActivitySearchResultBinding.inflate(layoutInflater)
     }
-    private val videosAdapter: VideosAdapter by lazy {
+    private val videosAdapter: VideosAdapter =
         VideosAdapter(
             object : VideosViewHolder.OnSearchResultListener {
                 override fun onSearchResultClick() {
@@ -21,12 +24,17 @@ class SearchResultActivity : BaseActivity<SearchResultViewModel, ActivitySearchR
                 }
             },
         )
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val region: String = intent.getStringExtra(REGION_ID) ?: return
+        viewModel.setRegionFromIntent(region)
+
         setupToolbar()
         setupAdapters()
+        setupBindings(region)
+        setupObservers()
     }
 
     private fun setupToolbar() {
@@ -43,8 +51,33 @@ class SearchResultActivity : BaseActivity<SearchResultViewModel, ActivitySearchR
         )
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun setupAdapters() {
         binding.rvSearchResult.adapter = videosAdapter
+    }
+
+    private fun setupBindings(region: String) {
+        val regionOrigin = RegionModel.findByEnglish(region)
+        binding.tbSearchResult.title = regionOrigin.korean
+    }
+
+    private fun setupObservers() {
+        viewModel.searchResultState.observe(this) { searchResultState: SearchResultViewModel.SearchResultState? ->
+            binding.tvSearchResultCount.text =
+                getString(
+                    R.string.search_result_exist_result,
+                    searchResultState?.searchResultCount ?: 0,
+                )
+
+            videosAdapter.submitList(searchResultState?.videos)
+        }
     }
 
     companion object {
