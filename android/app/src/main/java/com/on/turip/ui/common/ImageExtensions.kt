3e290bf8a563c5cb85b61110abfe84c9c84562c1
yrsel <xxknu88@gmail.com>
2025-07-23 18:15:13 +0900

    refactor: 이미지 불러오기 확장함수명 변경

--- android/app/src/main/java/com/on/turip/ui/common/ImageExtensions.kt
+++ android/app/src/main/java/com/on/turip/ui/common/ImageExtensions.kt
@@ -4,7 +4,7 @@ import android.widget.ImageView
 import coil.load
 import coil.transform.CircleCropTransformation
 
-fun ImageView.loadByCircle(imageUrl: String) {
+fun ImageView.loadCircularImage(imageUrl: String) {
     this.load(imageUrl) {
         crossfade(true)
         transformations(CircleCropTransformation())
