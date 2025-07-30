c195a7990ab1afd379b837d9c2484103b9c0d893
yrsel <xxknu88@gmail.com>
2025-07-30 23:49:36 +0900

    feat: Turip 전용 Url converter 생성

---
+++ android/app/src/main/java/com/on/turip/ui/common/TuripUrlConverter.kt
@@ -0,0 +1,16 @@
+package com.on.turip.ui.common
+
+object TuripUrlConverter {
+    private const val VIDEO_ID_POSITION = 1
+
+    private object VideoRegex {
+        val patternVParam = "v=([a-zA-Z0-9_-]{11})(?:&|$)".toRegex()
+        val patternShortUrl = "youtu\\.be/([a-zA-Z0-9_-]{11})".toRegex()
+    }
+
+    fun extractVideoId(videoUrl: String): String {
+        val matches =
+            VideoRegex.patternVParam.find(videoUrl) ?: VideoRegex.patternShortUrl.find(videoUrl)
+        return matches?.groups?.get(VIDEO_ID_POSITION)?.value ?: ""
+    }
+}
