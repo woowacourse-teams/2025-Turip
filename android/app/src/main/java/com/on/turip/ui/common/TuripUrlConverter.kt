c0aa515227b14c2552b77c96579ec662eff2d9cf
yrsel <xxknu88@gmail.com>
2025-07-31 11:04:09 +0900

    refactor: Video Id 찾기 위한 pattern 변수명 수정

--- android/app/src/main/java/com/on/turip/ui/common/TuripUrlConverter.kt
+++ android/app/src/main/java/com/on/turip/ui/common/TuripUrlConverter.kt
@@ -5,13 +5,14 @@ object TuripUrlConverter {
     private const val VIDEO_ID_POSITION: Int = 1
 
     private object VideoRegex {
-        val patternVParam: Regex = "v=([a-zA-Z0-9_-]{11})(?:&|$)".toRegex()
+        val patternVideoPath: Regex = "v=([a-zA-Z0-9_-]{11})(?:&|$)".toRegex()
         val patternShortUrl: Regex = "youtu\\.be/([a-zA-Z0-9_-]{11})".toRegex()
     }
 
     fun extractVideoId(videoUrl: String): String {
         val matches: MatchResult? =
-            VideoRegex.patternVParam.find(videoUrl) ?: VideoRegex.patternShortUrl.find(videoUrl)
+            VideoRegex.patternVideoPath.find(videoUrl)
+                ?: VideoRegex.patternShortUrl.find(videoUrl)
         return matches?.groups?.get(VIDEO_ID_POSITION)?.value ?: ""
     }
 
