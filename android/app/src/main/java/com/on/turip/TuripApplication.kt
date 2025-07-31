137eb110a02d7ffacdace861378b790ee52b458a
m6z1 <sonmyungj1zz@gmail.com>
2025-08-01 00:16:34 +0900

    feat: Timber 배포 / 디버깅 버전 트리 분기 처리

--- android/app/src/main/java/com/on/turip/TuripApplication.kt
+++ android/app/src/main/java/com/on/turip/TuripApplication.kt
@@ -1,5 +1,17 @@
 package com.on.turip
 
 import android.app.Application
+import com.on.turip.common.TuripReleaseTree
+import timber.log.Timber
 
-class TuripApplication : Application()
+class TuripApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        } else {
+            Timber.plant(TuripReleaseTree())
+        }
+    }
+}
