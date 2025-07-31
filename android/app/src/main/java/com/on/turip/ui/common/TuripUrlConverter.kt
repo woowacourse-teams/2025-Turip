edd393b158939e8cf7ddfc04e0b517dec23300d4
yrsel <xxknu88@gmail.com>
2025-07-31 10:57:35 +0900

    refactor: 값에 대한 타입 명시

--- android/app/src/main/java/com/on/turip/ui/common/TuripUrlConverter.kt
+++ android/app/src/main/java/com/on/turip/ui/common/TuripUrlConverter.kt
@@ -1,22 +1,22 @@
 package com.on.turip.ui.common
 
 object TuripUrlConverter {
-    private const val YOUTUBE_VIDEO_THUMBNAIL_FORM = "https://img.youtube.com/vi/%s/0.jpg"
-    private const val VIDEO_ID_POSITION = 1
+    private const val YOUTUBE_VIDEO_THUMBNAIL_FORM: String = "https://img.youtube.com/vi/%s/0.jpg"
+    private const val VIDEO_ID_POSITION: Int = 1
 
     private object VideoRegex {
-        val patternVParam = "v=([a-zA-Z0-9_-]{11})(?:&|$)".toRegex()
-        val patternShortUrl = "youtu\\.be/([a-zA-Z0-9_-]{11})".toRegex()
+        val patternVParam: Regex = "v=([a-zA-Z0-9_-]{11})(?:&|$)".toRegex()
+        val patternShortUrl: Regex = "youtu\\.be/([a-zA-Z0-9_-]{11})".toRegex()
     }
 
     fun extractVideoId(videoUrl: String): String {
-        val matches =
+        val matches: MatchResult? =
             VideoRegex.patternVParam.find(videoUrl) ?: VideoRegex.patternShortUrl.find(videoUrl)
         return matches?.groups?.get(VIDEO_ID_POSITION)?.value ?: ""
     }
 
     fun toVideoThumbnailUrl(videoUrl: String): String {
-        val videoId = extractVideoId(videoUrl)
+        val videoId: String = extractVideoId(videoUrl)
         return YOUTUBE_VIDEO_THUMBNAIL_FORM.format(videoId)
     }
 }
