ca5b413f6a71304ac816691a337b1876734f06be
jerry8282 <ka06196@naver.com>
2025-08-02 20:14:42 +0900

    feat: 키워드로 검색 화면 추가

---
+++ android/app/src/main/java/com/on/turip/ui/search/keywordresult/SearchResultActivity.kt
@@ -0,0 +1,21 @@
+package com.on.turip.ui.search.keywordresult
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.on.turip.R
+
+class SearchResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_search_result)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
