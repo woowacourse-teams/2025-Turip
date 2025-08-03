4cde9aefa48bb5caf2a499e35426a08607b625fc
yrsel <xxknu88@gmail.com>
2025-08-04 00:28:24 +0900

    refactor: ktlint에 맞게 고유하지만 상수가 아닌 프로퍼티명 수정

--- android/app/src/main/java/com/on/turip/data/settingsStorage/dataSource/DefaultSettingsStorageLocalDataSource.kt
+++ android/app/src/main/java/com/on/turip/data/settingsStorage/dataSource/DefaultSettingsStorageLocalDataSource.kt
@@ -7,25 +7,25 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
-import kotlin.coroutines.CoroutineContext
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
 
 class DefaultSettingsStorageLocalDataSource(
     private val context: Context,
     private val coroutineContext: CoroutineContext = Dispatchers.IO,
 ) : SettingsStorageLocalDataSource {
     private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings_storage")
-    private val DEVICE_FID_KEY: Preferences.Key<String> = stringPreferencesKey("device_fid")
+    private val deviceFIDKey: Preferences.Key<String> = stringPreferencesKey("device_fid")
 
     override suspend fun createId(id: String): Unit =
         withContext(coroutineContext) {
-            context.dataStore.edit { prefs: MutablePreferences -> prefs[DEVICE_FID_KEY] = id }
+            context.dataStore.edit { prefs: MutablePreferences -> prefs[deviceFIDKey] = id }
         }
 
     override suspend fun getId(): Result<String?> =
         withContext(coroutineContext) {
-            runCatching { context.dataStore.data.first()[DEVICE_FID_KEY] }
+            runCatching { context.dataStore.data.first()[deviceFIDKey] }
         }
 }
