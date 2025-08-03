4aa5fae3abbd0c1bf121d8335106d47d0cbb2894
RaZel713 <razel7130@gmail.com>
2025-08-03 15:46:10 +0900

    feat: 국내외 지역 카테고리 추가

---
+++ backend/src/main/java/turip/regioncategory/domain/DomesticRegionCategory.java
@@ -0,0 +1,39 @@
+package turip.regioncategory.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum DomesticRegionCategory {
+    SEOUL("서울"),
+    BUSAN("부산"),
+    JEJU("제주"),
+    INCHEON("인천"),
+    DAEJEON("대전"),
+    JEONJU("전주"),
+    GANGNEUNG("강릉"),
+    SOKCHO("속초"),
+    GYEONGJU("경주"),
+    OTHER_DOMESTIC("국내 기타");
+
+    private final String displayName;
+
+    public static boolean containsName(String name) {
+        for (DomesticRegionCategory category : values()) {
+            if (name.contains(category.displayName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static List<String> getDisplayNamesExcludingEtc() {
+        return Arrays.stream(values())
+                .filter(category -> !category.equals(OTHER_DOMESTIC))
+                .map(DomesticRegionCategory::getDisplayName)
+                .toList();
+    }
+}
---
+++ backend/src/main/java/turip/regioncategory/domain/OverseasRegionCategory.java
@@ -0,0 +1,35 @@
+package turip.regioncategory.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+public enum OverseasRegionCategory {
+    JAPAN("일본"),
+    CHINA("중국"),
+    VIETNAM("베트남"),
+    TAIWAN("대만"),
+    OTHER_OVERSEAS("해외 기타");
+
+    private final String displayName;
+
+    public static boolean containsName(String name) {
+        for (OverseasRegionCategory category : values()) {
+            if (name.contains(category.displayName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static List<String> getDisplayNamesExcludingEtc() {
+        return Arrays.stream(values())
+                .filter(category -> !category.equals(OTHER_OVERSEAS))
+                .map(OverseasRegionCategory::getDisplayName)
+                .toList();
+    }
+}
