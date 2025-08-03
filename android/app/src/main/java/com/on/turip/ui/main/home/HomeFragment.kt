d3dcd88dd768f37a1c1eacc11fcb52ae1ad921f2
m6z1 <sonmyungj1zz@gmail.com>
2025-08-04 01:13:46 +0900

    feat: 기존 불필요한 코드 제거

--- android/app/src/main/java/com/on/turip/ui/main/home/HomeFragment.kt
+++ android/app/src/main/java/com/on/turip/ui/main/home/HomeFragment.kt
@@ -13,45 +13,36 @@ import com.on.turip.R
 import com.on.turip.databinding.FragmentHomeBinding
 import com.on.turip.ui.common.base.BaseFragment
 import com.on.turip.ui.common.model.RegionModel
-import com.on.turip.ui.main.RegionAdapter
 import com.on.turip.ui.search.result.SearchResultActivity
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     private val viewModel: HomeViewModel by viewModels()
 
-    private val metropolitanCitiesAdapter: RegionAdapter =
+    private val regionAdapter: RegionAdapter =
         RegionAdapter { region: RegionModel ->
             val intent = SearchResultActivity.newIntent(requireActivity(), region.english)
             startActivity(intent)
         }
-    private val provincesAdapter: RegionAdapter =
-        RegionAdapter { region: RegionModel ->
-            val intent = SearchResultActivity.newIntent(requireActivity(), region.english)
-            startActivity(intent)
-        }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View {
-        val view = super.onCreateView(inflater, container, savedInstanceState)
-
-        setupTextHighlighting()
-        setupAdapters()
-        setupObservers()
-
-        return view
-    }
 
     override fun inflateBinding(
         inflater: LayoutInflater,
         container: ViewGroup?,
     ): FragmentHomeBinding = FragmentHomeBinding.inflate(inflater, container, false)
 
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupTextHighlighting()
+        setupAdapters()
+        setupObservers()
+    }
+
     private fun setupTextHighlighting() {
-        val originalText: String = getString(R.string.main_where_should_we_go_title)
-        val highlightText: String = getString(R.string.main_where_should_we_go_highlighting)
+        val originalText: String = getString(R.string.home_where_should_we_go_title)
+        val highlightText: String = getString(R.string.home_where_should_we_go_highlighting)
         val startIndex: Int = originalText.indexOf(highlightText)
         val endIndex: Int = startIndex + highlightText.length
 
@@ -60,7 +51,7 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
                 setSpan(
                     BackgroundColorSpan(
                         ContextCompat.getColor(
-                            requireActivity(),
+                            requireContext(),
                             R.color.turip_lemon_faff60_50,
                         ),
                     ),
@@ -74,21 +65,12 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     }
 
     private fun setupAdapters() {
-        binding.rvHomeMetropolitanCity.adapter = metropolitanCitiesAdapter
-        binding.rvHomeProvince.adapter = provincesAdapter
+        binding.rvHomeRegion.adapter = regionAdapter
     }
 
     private fun setupObservers() {
         viewModel.metropolitanCities.observe(viewLifecycleOwner) { metropolitanCities: List<RegionModel> ->
-            metropolitanCitiesAdapter.submitList(metropolitanCities)
+            regionAdapter.submitList(metropolitanCities)
         }
-
-        viewModel.provinces.observe(viewLifecycleOwner) { provinces: List<RegionModel> ->
-            provincesAdapter.submitList(provinces)
-        }
-    }
-
-    companion object {
-        fun newInstance() = HomeFragment()
     }
 }
