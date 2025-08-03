0b9fea5a0ff93856d451bedac6f8e619b2742de1
yrsel <xxknu88@gmail.com>
2025-08-04 00:25:20 +0900

    feat: Firebase에서 ID 가져오는 초기화 담당 객체 생성

---
+++ android/app/src/main/java/com/on/turip/initializer/FirebaseInstallationsInitializer.kt
@@ -0,0 +1,23 @@
+package com.on.turip.initializer
+
+import com.google.firebase.installations.FirebaseInstallations
+import com.on.turip.domain.settingsStorage.repository.SettingsStorageRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class FirebaseInstallationsInitializer(
+    private val repository: SettingsStorageRepository,
+) {
+    fun setupFirebaseInstallationId(scope: CoroutineScope = CoroutineScope(Dispatchers.IO)) {
+        FirebaseInstallations.getInstance().id.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                scope.launch {
+                    repository.createId(task.result)
+                }
+            } else {
+                // TODO : Firebase Installation ID 가져오지 못했을 때
+            }
+        }
+    }
+}
