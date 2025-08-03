b91615b910a96a57f6d01c886b948ea49a841635
RaZel713 <razel7130@gmail.com>
2025-08-03 16:48:36 +0900

    feat: 국내외 지역 카테고리 추가

---
+++ backend/src/main/java/turip/regioncategory/domain/OverseasRegionCategory.java
@@ -0,0 +1,34 @@
+package turip.regioncategory.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum OverseasRegionCategory {
+    JAPAN("일본"),
+    CHINA("중국"),
+    VIETNAM("베트남"),
+    TAIWAN("대만"),
+    OTHER_OVERSEAS("해외 기타");
+
+    private final String displayName;
+
+    public static boolean containsName(String name) {
+        for (OverseasRegionCategory category : values()) {
+            if (name.contains(category.displayName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static List<String> getDisplayNamesExcludingEtc() {
+        return Arrays.stream(values())
+                .filter(category -> !category.equals(OTHER_OVERSEAS))
+                .map(OverseasRegionCategory::getDisplayName)
+                .toList();
+    }
+}
