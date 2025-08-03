abdbcec687c2cdb293f351df476ab64429b5027f
m6z1 <sonmyungj1zz@gmail.com>
2025-08-03 22:23:38 +0900

    refactor: DebugTree의 반환 값 non-nullable 하도록 개선

--- android/app/src/main/java/com/on/turip/common/TuripDebugTree.kt
+++ android/app/src/main/java/com/on/turip/common/TuripDebugTree.kt
@@ -3,6 +3,6 @@ package com.on.turip.common
 import timber.log.Timber
 
 class TuripDebugTree : Timber.DebugTree() {
-    override fun createStackElementTag(element: StackTraceElement): String? =
+    override fun createStackElementTag(element: StackTraceElement): String =
         "moongjenut ${element.className}:${element.lineNumber}#${element.methodName}"
 }
