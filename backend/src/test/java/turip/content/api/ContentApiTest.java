a276f08d5c9e8b47b8d629177ea9ac76812cf624
eunseongu <rndmstjs75@gmail.com>
2025-08-03 23:22:48 +0900

    fix: 충돌 해결

--- backend/src/test/java/turip/content/api/ContentApiTest.java
+++ backend/src/test/java/turip/content/api/ContentApiTest.java
@@ -34,6 +34,8 @@ public class ContentApiTest {
         jdbcTemplate.update("DELETE FROM city");
         jdbcTemplate.update("DELETE FROM country");
         jdbcTemplate.update("DELETE FROM province");
+        jdbcTemplate.update("DELETE FROM favorite");
+        jdbcTemplate.update("DELETE FROM member");
 
         jdbcTemplate.update("ALTER TABLE trip_course ALTER COLUMN id RESTART WITH 1");
         jdbcTemplate.update("ALTER TABLE place ALTER COLUMN id RESTART WITH 1");
@@ -44,6 +46,8 @@ public class ContentApiTest {
         jdbcTemplate.update("ALTER TABLE province ALTER COLUMN id RESTART WITH 1");
         jdbcTemplate.update("ALTER TABLE category ALTER COLUMN id RESTART WITH 1");
         jdbcTemplate.update("ALTER TABLE place_category ALTER COLUMN id RESTART WITH 1");
+        jdbcTemplate.update("ALTER TABLE favorite ALTER COLUMN id RESTART WITH 1");
+        jdbcTemplate.update("ALTER TABLE member ALTER COLUMN id RESTART WITH 1");
     }
 
     @DisplayName("/contents/count GET 컨텐츠 수 조회 테스트")
@@ -73,20 +77,20 @@ public class ContentApiTest {
         }
     }
 
-    @DisplayName("/contents/{id} GET 컨텐츠 단건 조회 테스트")
+    @DisplayName("/contents/{contentId} GET 컨텐츠 단건 조회 테스트")
     @Nested
-    class readContentById {
-        @DisplayName("id로 컨텐츠 단건 조회 성공 시 200 OK 코드와 컨텐츠 정보를 응답한다")
+    class readContent {
+        @DisplayName("contentId로 컨텐츠 단건 조회 성공 시 200 OK 코드와 컨텐츠 정보를 응답한다")
         @Test
         void readContentById() {
             // given
             jdbcTemplate.update(
-                    "INSERT INTO Creator (profile_image, channel_name) VALUES (?, ?)",
+                    "INSERT INTO creator (profile_image, channel_name) VALUES (?, ?)",
                     "https://image.example.com/creator1.jpg", "TravelMate");
             jdbcTemplate.update("INSERT INTO country (name) VALUES ('korea')");
             jdbcTemplate.update("INSERT INTO city (name, country_id) VALUES ('seoul', 1)");
             jdbcTemplate.update(
-                    "INSERT INTO Content (creator_id, city_id, url, title, uploaded_date) VALUES (?, ?, ?, ?, ?)",
+                    "INSERT INTO content (creator_id, city_id, url, title, uploaded_date) VALUES (?, ?, ?, ?, ?)",
                     1, 1, "https://youtube.com/watch?v=abcd1", "서울 데이트 코스 추천", "2024-07-01");
 
             // when & then
@@ -99,7 +103,40 @@ public class ContentApiTest {
                     .body("cityId", is(1))
                     .body("title", is("서울 데이트 코스 추천"))
                     .body("url", is("https://youtube.com/watch?v=abcd1"))
-                    .body("uploadedDate", is("2024-07-01"));
+                    .body("uploadedDate", is("2024-07-01"))
+                    .body("isFavorite", is(false));
+        }
+
+        @DisplayName("device-fid 헤더가 존재하며 찜이 되어 있는 경우 isFavorite이 true로 응답된다")
+        @Test
+        void readContentById_withDeviceFidHeader() {
+            // given
+            jdbcTemplate.update(
+                    "INSERT INTO creator (profile_image, channel_name) VALUES (?, ?)",
+                    "https://image.example.com/creator1.jpg", "TravelMate");
+            jdbcTemplate.update("INSERT INTO country (name) VALUES ('korea')");
+            jdbcTemplate.update("INSERT INTO city (name, country_id) VALUES ('seoul', 1)");
+            jdbcTemplate.update(
+                    "INSERT INTO content (creator_id, city_id, url, title, uploaded_date) VALUES (?, ?, ?, ?, ?)",
+                    1, 1, "https://youtube.com/watch?v=abcd1", "서울 데이트 코스 추천", "2024-07-01");
+            jdbcTemplate.update(
+                    "INSERT INTO member (device_fid) VALUES (?)", "testDeviceFid");
+            jdbcTemplate.update(
+                    "INSERT INTO favorite (member_id, content_id) VALUES (?, ?)", 1, 1);
+
+            // when & then
+            RestAssured.given().port(port)
+                    .header("device-fid", "testDeviceFid")
+                    .when().get("/contents/{id}", 1)
+                    .then()
+                    .statusCode(200)
+                    .body("id", is(1))
+                    .body("creatorId", is(1))
+                    .body("cityId", is(1))
+                    .body("title", is("서울 데이트 코스 추천"))
+                    .body("url", is("https://youtube.com/watch?v=abcd1"))
+                    .body("uploadedDate", is("2024-07-01"))
+                    .body("isFavorite", is(true));
         }
     }
 }
