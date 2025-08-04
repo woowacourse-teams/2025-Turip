85f180856d6fb9d75bbb5baf0edeb1363b90b587
yrsel <xxknu88@gmail.com>
2025-08-04 21:06:31 +0900

    refactor: firebase installation id 생성하는 메서드 매개변수명 fid로 수정

--- android/app/src/main/java/com/on/turip/data/userStorage/dataSource/DefaultUserStorageLocalDataSource.kt
+++ android/app/src/main/java/com/on/turip/data/userStorage/dataSource/DefaultUserStorageLocalDataSource.kt
@@ -7,10 +7,10 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
-import kotlin.coroutines.CoroutineContext
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
 
 class DefaultUserStorageLocalDataSource(
     private val context: Context,
@@ -19,9 +19,9 @@ class DefaultUserStorageLocalDataSource(
     private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "user_storage")
     private val deviceFIDKey: Preferences.Key<String> = stringPreferencesKey("device_fid")
 
-    override suspend fun createId(id: String): Unit =
+    override suspend fun createId(fid: String): Unit =
         withContext(coroutineContext) {
-            context.dataStore.edit { prefs: MutablePreferences -> prefs[deviceFIDKey] = id }
+            context.dataStore.edit { prefs: MutablePreferences -> prefs[deviceFIDKey] = fid }
         }
 
     override suspend fun getId(): Result<String?> =
