082d698b57b76bf43baf156013c9488423cea54d
eunseongu <rndmstjs75@gmail.com>
2025-08-05 16:16:28 +0900

    refactor: 불필요한 테스트 제거

--- backend/src/test/java/turip/content/api/ContentApiTest.java
+++ backend/src/test/java/turip/content/api/ContentApiTest.java
@@ -48,33 +48,6 @@ public class ContentApiTest {
         jdbcTemplate.update("ALTER TABLE member ALTER COLUMN id RESTART WITH 1");
     }
 
-    @DisplayName("/contents/count GET 지역별 컨텐츠 수 조회 테스트")
-    @Nested
-    class ReadCountByCityName {
-
-        @DisplayName("지역별 컨텐츠 수 조회 성공 시 200 OK 코드와 컨텐츠 수를 응답한다")
-        @Test
-        void readCountByCityName1() {
-            // given
-            jdbcTemplate.update(
-                    "INSERT INTO creator (profile_image, channel_name) VALUES ('https://image.example.com/creator1.jpg', 'TravelMate')");
-            jdbcTemplate.update("INSERT INTO country (name) VALUES ('대한민국')");
-            jdbcTemplate.update("INSERT INTO city (name, country_id) VALUES ('서울', 1)");
-            jdbcTemplate.update(
-                    "INSERT INTO content (creator_id, city_id, url, title, uploaded_date) VALUES (1, 1, 'https://youtube.com/watch?v=abcd1', '서울 데이트 코스 추천', '2024-07-01')");
-            jdbcTemplate.update(
-                    "INSERT INTO content (creator_id, city_id, url, title, uploaded_date) VALUES (1, 1, 'https://youtube.com/watch?v=abcd1', '서촌 당일치기 코스 추천', '2025-06-18')");
-
-            // when & then
-            RestAssured.given().port(port)
-                    .queryParam("cityName", "서울")
-                    .when().get("/contents/count")
-                    .then()
-                    .statusCode(200)
-                    .body("count", is(2));
-        }
-    }
-
     @DisplayName("/contents/{contentId} GET 컨텐츠 단건 조회 테스트")
     @Nested
     class ReadContentById {
