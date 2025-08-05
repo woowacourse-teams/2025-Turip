dd90711e65e3fbddb74d1270466fb436cec7749c
RaZel713 <razel7130@gmail.com>
2025-08-05 14:13:58 +0900

    refactor: merge 관련 처리

--- backend/src/main/java/turip/regioncategory/domain/OverseasRegionCategory.java
+++ backend/src/main/java/turip/regioncategory/domain/OverseasRegionCategory.java
@@ -1,9 +1,11 @@
 package turip.regioncategory.domain;
 
+import io.micrometer.common.util.StringUtils;
 import java.util.Arrays;
 import java.util.List;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import turip.exception.BadRequestException;
 
 @Getter
 @RequiredArgsConstructor
@@ -15,6 +17,19 @@ public enum OverseasRegionCategory {
     OTHER_OVERSEAS("해외 기타");
 
     private final String displayName;
+
+    public static boolean containsName(String regionCategoryName) {
+        if (StringUtils.isBlank(regionCategoryName)) {
+            throw new BadRequestException("지역 카테고리 명이 빈 값입니다.");
+        }
+
+        for (OverseasRegionCategory category : values()) {
+            if (regionCategoryName.contains(category.displayName)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     public static List<String> getDisplayNamesExcludingEtc() {
         return Arrays.stream(values())
