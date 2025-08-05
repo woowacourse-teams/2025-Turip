819faf96ea147f14f44e5e69c65588975fe23b84
RaZel713 <razel7130@gmail.com>
2025-08-05 14:12:18 +0900

    refactor: merge 관련 처리

--- backend/src/main/java/turip/regioncategory/domain/DomesticRegionCategory.java
+++ backend/src/main/java/turip/regioncategory/domain/DomesticRegionCategory.java
@@ -1,5 +1,8 @@
 package turip.regioncategory.domain;
 
+import io.micrometer.common.util.StringUtils;
+import java.util.Arrays;
+import java.util.List;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import turip.exception.BadRequestException;
