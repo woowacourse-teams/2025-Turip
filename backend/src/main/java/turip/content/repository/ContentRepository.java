1d9b8e29ecff009ae67576848674c96cc799717d
RaZel713 <razel7130@gmail.com>
2025-08-05 15:56:57 +0900

    refactor: repository 메서드 정렬 순서 변경

--- backend/src/main/java/turip/content/repository/ContentRepository.java
+++ backend/src/main/java/turip/content/repository/ContentRepository.java
@@ -10,7 +10,6 @@ import turip.content.domain.Content;
 
 public interface ContentRepository extends JpaRepository<Content, Long> {
 
-    // 국내 카테고리 컨텐츠 수 조회
     int countByCityName(String cityName);
 
     @Query("""
@@ -21,7 +20,6 @@ public interface ContentRepository extends JpaRepository<Content, Long> {
             """)
     int countDomesticEtcContents(List<String> cityNames);
 
-    // 해외 카테고리 컨텐츠 수 조회
     int countByCityCountryName(@Param("countryName") String countryName);
 
     @Query("""
@@ -32,17 +30,10 @@ public interface ContentRepository extends JpaRepository<Content, Long> {
             """)
     int countOverseasEtcContents(@Param("countryNames") List<String> countryNames);
 
-    // 국내 카테고리
     Slice<Content> findByCityNameOrderByIdDesc(String cityName, Pageable pageable);
 
     Slice<Content> findByCityNameAndIdLessThanOrderByIdDesc(String cityName, Long lastId, Pageable pageable);
 
-    // 해외 카테고리
-    Slice<Content> findByCityCountryNameOrderByIdDesc(String countryName, Pageable pageable);
-
-    Slice<Content> findByCityCountryNameAndIdLessThanOrderByIdDesc(String countryName, Long lastId, Pageable pageable);
-
-    // 국내 기타 카테고리: city 이름이 domesticCategoryNames 안에 없어야 하고, country 이름이 "대한민국"인 경우
     @Query("""
             SELECT c FROM Content c JOIN c.city ct JOIN ct.country co
             WHERE ct.name NOT IN :domesticCategoryNames AND co.name = '대한민국'
@@ -59,7 +50,10 @@ public interface ContentRepository extends JpaRepository<Content, Long> {
     Slice<Content> findDomesticEtcContentsWithLastId(@Param("domesticCategoryNames") List<String> domesticCategoryNames,
                                                      @Param("lastId") Long lastId, Pageable pageable);
 
-    // 해외 기타 카테고리: country 이름이 overseasCategoryNames 안에 없어야 하고, country 이름이 "대한민국"가 아닌 경우
+    Slice<Content> findByCityCountryNameOrderByIdDesc(String countryName, Pageable pageable);
+
+    Slice<Content> findByCityCountryNameAndIdLessThanOrderByIdDesc(String countryName, Long lastId, Pageable pageable);
+
     @Query("""
             SELECT c FROM Content c JOIN c.city ct JOIN ct.country co
             WHERE co.name NOT IN :overseasCategoryNames AND co.name != '대한민국'
