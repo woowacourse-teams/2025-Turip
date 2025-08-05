f016496a8224cf7483369d6e3b3874dfcf1d26fa
seaniiio <cnc4750@naver.com>
2025-08-05 20:35:45 +0900

    refactor: 애노테이션 순서를 컨벤션에 맞게 수정

--- backend/src/main/java/turip/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/turip/exception/GlobalExceptionHandler.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import turip.exception.custom.HttpStatusException;
 
-@RestControllerAdvice
 @Slf4j
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(HttpStatusException.class)
