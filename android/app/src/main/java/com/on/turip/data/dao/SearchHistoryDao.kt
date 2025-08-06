b326357ab0b5a2de192af0bbaf1bba24592a5b74
jerry8282 <ka06196@naver.com>
2025-08-06 15:27:44 +0900

    feat: Room Database 및 DAO 구현

---
+++ android/app/src/main/java/com/on/turip/data/dao/SearchHistoryDao.kt
@@ -0,0 +1,31 @@
+package com.on.turip.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.on.turip.data.searchhistory.SearchHistoryEntity
+
+@Dao
+interface SearchHistoryDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSearchHistory(searchHistory: SearchHistoryEntity)
+
+    @Query("SELECT * FROM search_history ORDER BY history DESC LIMIT :limit")
+    suspend fun getRecentSearches(limit: Int): List<SearchHistoryEntity>
+
+    @Query("DELETE FROM search_history WHERE keyword = :keyword")
+    suspend fun deleteSearch(keyword: String)
+
+    @Query(
+        """
+    DELETE FROM search_history
+    WHERE keyword NOT IN (
+        SELECT keyword FROM search_history
+        ORDER BY history DESC
+        LIMIT :limit
+    )
+""",
+    )
+    suspend fun deleteOldSearchesExceptLatest(limit: Int)
+}
