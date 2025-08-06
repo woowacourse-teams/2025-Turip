2ec8e5d129b70f28fd737923d51874161687caac
jerry8282 <ka06196@naver.com>
2025-08-06 11:38:32 +0900

    feat: 최근 검색 결과 저장 Entity 생성

---
+++ android/app/src/main/java/com/on/turip/data/searchhistory/SearchHistoryEntity.kt
@@ -0,0 +1,13 @@
+package com.on.turip.data.searchhistory
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "search_history")
+data class SearchHistoryEntity(
+    @PrimaryKey
+    val keyword: String,
+    @ColumnInfo(name = "history")
+    val searchedAt: Long = System.currentTimeMillis(),
+)
