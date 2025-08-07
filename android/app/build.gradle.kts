c900264d9c0df7e3b0da8b7d47b4e209d46c87e8
jerry8282 <ka06196@naver.com>
2025-08-08 08:26:08 +0900

    refactor: 불필요한 Firebase Crashlytics 설정 제거

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,5 +1,4 @@
 import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
-import com.google.firebase.crashlytics.buildtools.gradle.CrashlyticsExtension
 
 plugins {
     alias(libs.plugins.android.application)
@@ -54,10 +53,6 @@ android {
             )
             signingConfig = signingConfigs.getByName("debug")
             manifestPlaceholders["appName"] = "튜립"
-            configure<CrashlyticsExtension> {
-                // Enable processing and uploading of native symbols to Firebase servers.
-                nativeSymbolUploadEnabled = true
-            }
         }
     }
 
