b2dc4a4ce88d86527252d45b91832aa2286485ae
seaniiio <cnc4750@naver.com>
2025-08-13 21:12:59 +0900

    test: 데이터 초기화에 누락된 테이블 추가

--- backend/src/test/java/turip/content/api/ContentCountApiTest.java
+++ backend/src/test/java/turip/content/api/ContentCountApiTest.java
@@ -24,14 +24,18 @@ class ContentCountApiTest {
     @BeforeEach
     void setUp() {
         jdbcTemplate.update("DELETE FROM trip_course");
+        jdbcTemplate.update("DELETE FROM place_category");
         jdbcTemplate.update("DELETE FROM place");
+        jdbcTemplate.update("DELETE FROM favorite_content");
         jdbcTemplate.update("DELETE FROM content");
         jdbcTemplate.update("DELETE FROM creator");
         jdbcTemplate.update("DELETE FROM city");
         jdbcTemplate.update("DELETE FROM country");
 
         jdbcTemplate.update("ALTER TABLE trip_course ALTER COLUMN id RESTART WITH 1");
+        jdbcTemplate.update("ALTER TABLE place_category ALTER COLUMN id RESTART WITH 1");
         jdbcTemplate.update("ALTER TABLE place ALTER COLUMN id RESTART WITH 1");
+        jdbcTemplate.update("ALTER TABLE favorite_content ALTER COLUMN id RESTART WITH 1");
         jdbcTemplate.update("ALTER TABLE content ALTER COLUMN id RESTART WITH 1");
         jdbcTemplate.update("ALTER TABLE creator ALTER COLUMN id RESTART WITH 1");
         jdbcTemplate.update("ALTER TABLE city ALTER COLUMN id RESTART WITH 1");
@@ -160,7 +164,7 @@ class ContentCountApiTest {
             jdbcTemplate.update("INSERT INTO city (name, country_id) VALUES ('도쿄', 1)");
             jdbcTemplate.update("INSERT INTO city (name, country_id) VALUES ('방콕', 2)");
             jdbcTemplate.update("INSERT INTO city (name, country_id) VALUES ('싱가포르시티', 3)");
-            
+
             // when & then
             RestAssured.given().port(port)
                     .queryParam("regionCategory", "아라키스")
