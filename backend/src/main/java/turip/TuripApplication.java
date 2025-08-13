c651bce299c1c9dc5cdc2d01946a8e5012d5734a
RaZel713 <razel7130@gmail.com>
2025-08-13 21:10:21 +0900

    feat: JPA 감사 기능 활성화를 위한 EnableJpaAuditing 추가

--- backend/src/main/java/turip/TuripApplication.java
+++ backend/src/main/java/turip/TuripApplication.java
@@ -2,8 +2,10 @@ package turip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TuripApplication {
 
     public static void main(String[] args) {
