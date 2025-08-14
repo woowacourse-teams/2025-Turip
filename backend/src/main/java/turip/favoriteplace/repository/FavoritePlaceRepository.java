72d6ac7357b7bf98d360b86d2ed99a981f2d4ea6
RaZel713 <razel7130@gmail.com>
2025-08-15 01:37:05 +0900

    refactor: 미사용 파일 삭제

--- backend/src/main/java/turip/favoriteplace/repository/FavoritePlaceRepository.java
+++
@@ -1,7 +0,0 @@
-package turip.favoriteplace.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import turip.favoriteplace.domain.FavoritePlace;
-
-public interface FavoritePlaceRepository extends JpaRepository<FavoritePlace, Long> {
-}
