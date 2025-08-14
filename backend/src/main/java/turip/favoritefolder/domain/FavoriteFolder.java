c2395887c5d461b03b1baf0146d21746dc3209b2
seaniiio <cnc4750@naver.com>
2025-08-14 15:29:54 +0900

    refactor: 현재 사용하지 않는 팩토리 메서드 삭제

--- backend/src/main/java/turip/favoritefolder/domain/FavoriteFolder.java
+++ backend/src/main/java/turip/favoritefolder/domain/FavoriteFolder.java
@@ -31,10 +31,6 @@ public class FavoriteFolder {
 
     private boolean isDefault;
 
-    public static FavoriteFolder defaultFolderOf(Member member, String name) {
-        return new FavoriteFolder(member, name, true);
-    }
-
     public static FavoriteFolder customFolderOf(Member member, String name) {
         return new FavoriteFolder(member, name, false);
     }
