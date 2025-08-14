7df82c7664df6ae5647b9d936b9353227c4f610a
RaZel713 <razel7130@gmail.com>
2025-08-14 13:25:23 +0900

    test: ContentPlaceServiceTest에서 타임 라인 반환 여부에 대한 테스트 추가

--- backend/src/test/java/turip/contentplace/service/ContentPlaceServiceTest.java
+++ backend/src/test/java/turip/contentplace/service/ContentPlaceServiceTest.java
@@ -84,6 +84,51 @@ class ContentPlaceServiceTest {
         assertThat(response.contentPlaceCount()).isEqualTo(2);
     }
 
+    @DisplayName("여행 코스 장소들의 타임 라인을 확인할 수 있다.")
+    @Test
+    void checkTimeLine() {
+        // given
+        int firstVisitDay = 1;
+        int secondVisitDay = 2;
+        int visitOrder = 1;
+        long contentId = 1L;
+
+        Place place1 = new Place(
+                1L,
+                "경포대 해수욕장",
+                "https://naver.me/5UrZAIeY",
+                "경포대 해수욕장의 도로명 주소",
+                38.1234,
+                127.23123
+        );
+        place1.addCategory(new Category("관광지"));
+        Place place2 = new Place(
+                2L,
+                "하우스멜",
+                "https://naver.me/test",
+                "하우스멜의 도로명 주소",
+                38.0001,
+                127.12345
+        );
+        place2.addCategory(new Category("빵집"));
+        place2.addCategory(new Category("카페"));
+
+        LocalTime firstDayTimeLine = LocalTime.parse("00:11:30");
+        LocalTime secondDayTimeLine = LocalTime.parse("00:13:30");
+        ContentPlace firstContentPlace = new ContentPlace(firstVisitDay, visitOrder, firstDayTimeLine, place1, null);
+        ContentPlace secondContentPlace = new ContentPlace(secondVisitDay, visitOrder, secondDayTimeLine, place2, null);
+
+        // when
+        given(contentPlaceRepository.findAllByContentId(contentId))
+                .willReturn(List.of(firstContentPlace, secondContentPlace));
+        ContentPlaceDetailResponse response = contentPlaceService.findContentPlaceDetails(contentId);
+
+        // then
+        assertThat(response.contentPlaces().get(0).timeLine()).isEqualTo(LocalTime.parse("00:11:30"));
+        assertThat(response.contentPlaces().get(1).timeLine()).isEqualTo(LocalTime.parse("00:13:30"));
+        ;
+    }
+
     @DisplayName("여행 코스의 여행 기간 계산")
     @Nested
     class CalculateDurationDays {
