b3551483f0d92482d47582f9007722a7c73bba1a
jerry8282 <ka06196@naver.com>
2025-08-14 14:34:06 +0900

    feat: 네트워크 응답 처리 클래스 추가

---
+++ android/app/src/main/java/com/on/turip/data/common/TuripCustomResult.kt
@@ -0,0 +1,48 @@
+package com.on.turip.data.common
+
+import timber.log.Timber
+
+sealed class TuripCustomResult<out T> {
+    data class Success<out T>(
+        val data: T,
+    ) : TuripCustomResult<T>()
+
+    data class HttpError(
+        private val statusCode: Int,
+    ) : TuripCustomResult<Nothing>() {
+        init {
+            when (statusCode) {
+                403 -> Timber.e("$statusCode : 사용자의 권한이 없습니다.")
+                404 -> Timber.e("$statusCode : 찾을 수 없음")
+                409 -> Timber.e("$statusCode : 폴더명이 중복입니다.")
+                500 -> Timber.e("$statusCode : 서버에서 예기치 않은 문제가 발생하였습니다.")
+                else -> Timber.e("찾을 수 없는 코드: $statusCode")
+            }
+        }
+
+        fun whatError(): HttpEvent =
+            when (statusCode) {
+                403 -> HttpEvent.USER_NOT_HAVE_PERMISSION
+                404 -> HttpEvent.NOT_FOUND
+                409 -> HttpEvent.DUPLICATION_FOLDER
+                500 -> HttpEvent.UNEXPECTED_PROBLEM
+                else -> HttpEvent.UNKNOWN
+            }
+    }
+
+    data class ParseError(
+        val exception: Throwable,
+    ) : TuripCustomResult<Nothing>() {
+        init {
+            Timber.e("파싱 에러: ${exception.message}")
+        }
+    }
+
+    data class NetworkError(
+        val exception: Throwable,
+    ) : TuripCustomResult<Nothing>() {
+        init {
+            Timber.e("네트워크 에러: ${exception.message}")
+        }
+    }
+}
