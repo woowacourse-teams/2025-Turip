40d5012f15a2eed4554ab74d22411e96e9fa01c4
yrsel <xxknu88@gmail.com>
2025-08-15 17:34:07 +0900

    feat: 찜 장소 폴더명 리사이클러뷰 구현

--- android/app/src/main/java/com/on/turip/ui/main/favorite/FavoritePlaceFragment.kt
+++ android/app/src/main/java/com/on/turip/ui/main/favorite/FavoritePlaceFragment.kt
@@ -2,15 +2,65 @@ package com.on.turip.ui.main.favorite
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.on.turip.R
+import androidx.recyclerview.widget.RecyclerView
+import com.on.turip.databinding.FragmentFavoritePlaceBinding
+import com.on.turip.ui.common.base.BaseFragment
+import com.on.turip.ui.main.favorite.model.FavoritePlaceFolderModel
 
-class FavoritePlaceFragment : Fragment() {
-    override fun onCreateView(
+class FavoritePlaceFragment : BaseFragment<FragmentFavoritePlaceBinding>() {
+    private val folderAdapter: FavoritePlaceFolderAdapter by lazy {
+        FavoritePlaceFolderAdapter { favoritePlaceFolderModel: FavoritePlaceFolderModel ->
+            // TODO: viewModel에서 선택한 찜 폴더에 해당하는 아이템을 불러오기
+        }
+    }
+
+    override fun inflateBinding(
         inflater: LayoutInflater,
         container: ViewGroup?,
+    ): FragmentFavoritePlaceBinding = FragmentFavoritePlaceBinding.inflate(inflater, container, false)
+
+    override fun onViewCreated(
+        view: View,
         savedInstanceState: Bundle?,
-    ): View? = inflater.inflate(R.layout.fragment_favorite_place, container, false)
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupAdapters()
+    }
+
+    private fun setupAdapters() {
+        binding.rvFavoritePlaceFolder.apply {
+            adapter = folderAdapter
+            addOnItemTouchListener(RecyclerViewTouchInterceptor)
+        }
+    }
+}
+
+private object RecyclerViewTouchInterceptor : RecyclerView.OnItemTouchListener {
+    override fun onInterceptTouchEvent(
+        recyclerView: RecyclerView,
+        motionEvent: MotionEvent,
+    ): Boolean {
+        when (motionEvent.action) {
+            MotionEvent.ACTION_DOWN -> {
+                recyclerView.parent.requestDisallowInterceptTouchEvent(true)
+            }
+
+            MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                recyclerView.parent.requestDisallowInterceptTouchEvent(false)
+            }
+        }
+        return false
+    }
+
+    override fun onTouchEvent(
+        recyclerView: RecyclerView,
+        motionEvent: MotionEvent,
+    ) {
+    }
+
+    override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {}
 }
