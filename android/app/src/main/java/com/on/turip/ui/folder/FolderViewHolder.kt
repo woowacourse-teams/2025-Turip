dff1e8c549f065c1651c11961010e4614e8fefdf
yrsel <xxknu88@gmail.com>
2025-08-16 12:37:46 +0900

    feat: 폴더 화면 리사이클러뷰 구현

---
+++ android/app/src/main/java/com/on/turip/ui/folder/FolderViewHolder.kt
@@ -0,0 +1,50 @@
+package com.on.turip.ui.folder
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.on.turip.R
+import com.on.turip.databinding.ItemFolderBinding
+import com.on.turip.ui.folder.model.FolderModel
+
+class FolderViewHolder(
+    private val binding: ItemFolderBinding,
+    folderListener: FolderListener,
+) : RecyclerView.ViewHolder(binding.root) {
+    private var folderModel: FolderModel? = null
+
+    init {
+        binding.ivFolderRemove.setOnClickListener {
+            folderModel?.let { folderListener.onRemoveClick(it.id) }
+        }
+
+        binding.clFolderItem.setOnClickListener {
+            folderModel?.let { folderListener.onItemClick(it.id) }
+        }
+    }
+
+    fun bind(folderModel: FolderModel) {
+        this.folderModel = folderModel
+
+        binding.tvFolderTitle.text = folderModel.title
+        binding.tvFolderPlaceCount.text =
+            itemView.context.getString(R.string.all_total_place_count, folderModel.count)
+    }
+
+    companion object {
+        fun of(
+            parent: ViewGroup,
+            folderListener: FolderListener,
+        ): FolderViewHolder {
+            val inflater: LayoutInflater = LayoutInflater.from(parent.context)
+            val binding: ItemFolderBinding = ItemFolderBinding.inflate(inflater, parent, false)
+            return FolderViewHolder(binding, folderListener)
+        }
+    }
+
+    interface FolderListener {
+        fun onRemoveClick(folderId: Long)
+
+        fun onItemClick(folderId: Long)
+    }
+}
