b43f401aa292e02ef6dcaf979a4b54052216bbee
RaZel713 <razel7130@gmail.com>
2025-08-18 03:00:39 +0900

    feat: City 도메인에 DB 조건 명시

--- backend/src/main/java/turip/city/domain/City.java
+++ backend/src/main/java/turip/city/domain/City.java
@@ -1,11 +1,14 @@
 package turip.city.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -15,6 +18,7 @@ import turip.province.domain.Province;
 
 @Getter
 @Entity
+@Table(name = "city")
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class City {
@@ -25,14 +29,17 @@ public class City {
     private Long id;
 
     @ManyToOne
+    @JoinColumn(name = "country_id", nullable = false, foreignKey = @ForeignKey(name = "fk_city_country"))
     private Country country;
 
     @ManyToOne
-    @JoinColumn(name = "province_id", nullable = true)
+    @JoinColumn(name = "province_id", foreignKey = @ForeignKey(name = "fk_city_province"))
     private Province province;
 
+    @Column(name = "name", nullable = false)
     private String name;
 
+    @Column(name = "image_url", nullable = false)
     private String imageUrl;
 
     public City(Country country, Province province, String name, String imageUrl) {
