8ac82f187e22ee4d4c5ca7f7052eb57c3ca36ca6
RaZel713 <razel7130@gmail.com>
2025-08-18 03:06:22 +0900

    feat: ContentPlace 도메인에 DB 조건 명시

--- backend/src/main/java/turip/contentplace/domain/ContentPlace.java
+++ backend/src/main/java/turip/contentplace/domain/ContentPlace.java
@@ -1,10 +1,15 @@
 package turip.contentplace.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -15,6 +20,9 @@ import java.time.LocalTime;
 
 @Getter
 @Entity
+@Table(name = "content_place", uniqueConstraints = {
+    @UniqueConstraint(name = "uq_content_place__content_day_order", columnNames = {"content_id", "visit_day", "visit_order"})
+})
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ContentPlace {
@@ -24,16 +32,21 @@ public class ContentPlace {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "visit_day", nullable = false)
     private int visitDay;
 
+    @Column(name = "visit_order", nullable = false)
     private int visitOrder;
 
+    @Column(name = "time_line", nullable = false)
     private LocalTime timeLine;
 
     @ManyToOne
+    @JoinColumn(name = "place_id", nullable = false, foreignKey = @ForeignKey(name = "fk_content_place__place"))
     private Place place;
 
     @ManyToOne
+    @JoinColumn(name = "content_id", nullable = false, foreignKey = @ForeignKey(name = "fk_content_place__content"))
     private Content content;
 
     public ContentPlace(int visitDay, int visitOrder, LocalTime timeLine, Place place, Content content) {
