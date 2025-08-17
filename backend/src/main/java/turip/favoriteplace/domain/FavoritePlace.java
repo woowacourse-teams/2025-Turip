1dc110f563962f598f527d6ce7778c1cff0b1ba2
RaZel713 <razel7130@gmail.com>
2025-08-18 03:07:41 +0900

    feat: FavoritePlace 도메인에 DB 조건 명시

--- backend/src/main/java/turip/favoriteplace/domain/FavoritePlace.java
+++ backend/src/main/java/turip/favoriteplace/domain/FavoritePlace.java
@@ -1,10 +1,14 @@
 package turip.favoriteplace.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -14,6 +18,9 @@ import turip.place.domain.Place;
 
 @Getter
 @Entity
+@Table(name = "favorite_place", uniqueConstraints = {
+    @UniqueConstraint(name = "uq_favorite_place__folder_place", columnNames = {"favorite_folder_id", "place_id"})
+})
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FavoritePlace {
@@ -24,8 +31,10 @@ public class FavoritePlace {
     private Long id;
 
     @ManyToOne
+    @JoinColumn(name = "favorite_folder_id", nullable = false, foreignKey = @ForeignKey(name = "fk_favorite_place__folder"))
     private FavoriteFolder favoriteFolder;
 
     @ManyToOne
+    @JoinColumn(name = "place_id", nullable = false, foreignKey = @ForeignKey(name = "fk_favorite_place__place"))
     private Place place;
 }
