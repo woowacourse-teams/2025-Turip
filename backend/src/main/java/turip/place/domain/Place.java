559ad05ce7c7c1cad1fdc2f3052e6c8c166265ef
RaZel713 <razel7130@gmail.com>
2025-08-18 03:04:48 +0900

    feat: Place 도메인에 DB 조건 명시

--- backend/src/main/java/turip/place/domain/Place.java
+++ backend/src/main/java/turip/place/domain/Place.java
@@ -1,11 +1,13 @@
 package turip.place.domain;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AccessLevel;
@@ -17,6 +19,7 @@ import turip.placecategory.domain.PlaceCategory;
 
 @Getter
 @Entity
+@Table(name = "place")
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Place {
@@ -26,14 +29,19 @@ public class Place {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "name", nullable = false)
     private String name;
 
+    @Column(name = "url", nullable = false, unique = true, length = 500)
     private String url;
 
+    @Column(name = "address", nullable = false)
     private String address;
 
+    @Column(name = "latitude", nullable = false)
     private double latitude;
 
+    @Column(name = "longitude", nullable = false)
     private double longitude;
 
     @OneToMany(mappedBy = "place", cascade = CascadeType.ALL, orphanRemoval = true)
