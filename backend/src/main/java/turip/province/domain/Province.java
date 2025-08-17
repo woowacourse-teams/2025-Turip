74db92f8986c4d1cefa91ad3d7351fb65f870270
RaZel713 <razel7130@gmail.com>
2025-08-18 02:58:44 +0900

    feat: Province 도메인에 DB 조건 명시

--- backend/src/main/java/turip/province/domain/Province.java
+++ backend/src/main/java/turip/province/domain/Province.java
@@ -1,9 +1,11 @@
 package turip.province.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -11,6 +13,7 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
+@Table(name = "province")
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Province {
@@ -20,6 +23,7 @@ public class Province {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "name", nullable = false)
     private String name;
 
     public Province(String name) {
