dedf9fee4da9f5455f01c118d4124e1fb12ad9fe
jerry8282 <ka06196@naver.com>
2025-08-19 15:40:04 +0900

    feat: 의존성 주입 객체에 추가

--- android/app/src/main/java/com/on/turip/data/place/repository/DefaultFavoritePlaceRepository.kt
+++ android/app/src/main/java/com/on/turip/data/place/repository/DefaultFavoritePlaceRepository.kt
@@ -2,13 +2,13 @@ package com.on.turip.data.place.repository
 
 import com.on.turip.data.common.TuripCustomResult
 import com.on.turip.data.common.mapCatching
-import com.on.turip.data.place.datasource.DefaultFavoritePlaceDataSource
+import com.on.turip.data.place.datasource.FavoritePlaceDataSource
 import com.on.turip.data.place.toDomain
 import com.on.turip.domain.favorite.repository.FavoritePlaceRepository
 import com.on.turip.domain.trip.Place
 
 class DefaultFavoritePlaceRepository(
-    private val favoritePlaceDataSource: DefaultFavoritePlaceDataSource,
+    private val favoritePlaceDataSource: FavoritePlaceDataSource,
 ) : FavoritePlaceRepository {
     override suspend fun loadFavoritePlaces(favoriteFolderId: Long): TuripCustomResult<List<Place>> =
         favoritePlaceDataSource.getFavoritePlaces(favoriteFolderId).mapCatching {
--- android/app/src/main/java/com/on/turip/di/DataSourceModule.kt
+++ android/app/src/main/java/com/on/turip/di/DataSourceModule.kt
@@ -10,6 +10,8 @@ import com.on.turip.data.favorite.datasource.DefaultFavoriteRemoteDataSource
 import com.on.turip.data.favorite.datasource.FavoriteRemoteDataSource
 import com.on.turip.data.folder.datasource.DefaultFolderRemoteDataSource
 import com.on.turip.data.folder.datasource.FolderRemoteDataSource
+import com.on.turip.data.place.datasource.DefaultFavoritePlaceDataSource
+import com.on.turip.data.place.datasource.FavoritePlaceDataSource
 import com.on.turip.data.region.datasource.DefaultRegionRemoteDataSource
 import com.on.turip.data.region.datasource.RegionRemoteDataSource
 import com.on.turip.data.searchhistory.datasource.DefaultSearchHistoryDataSource
@@ -41,5 +43,8 @@ object DataSourceModule {
     }
     val folderRemoteDataSource: FolderRemoteDataSource by lazy {
         DefaultFolderRemoteDataSource(NetworkModule.folderService)
+    }
+    val favoritePlaceDataSource: FavoritePlaceDataSource by lazy {
+        DefaultFavoritePlaceDataSource(NetworkModule.favoritePlaceService)
     }
 }
