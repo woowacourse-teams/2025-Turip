4c54080f243e06f6a6e0a8b98bad3c026fdbf1e3
eunseongu <rndmstjs75@gmail.com>
2025-09-12 17:48:48 +0900

    refactor: 불필요한 라인 제거 및 컨벤션 통일

--- backend/src/main/java/turip/favorite/controller/dto/response/FavoriteFoldersWithFavoriteStatusResponse.java
+++ backend/src/main/java/turip/favorite/controller/dto/response/FavoriteFoldersWithFavoriteStatusResponse.java
@@ -3,7 +3,8 @@ package turip.favorite.controller.dto.response;
 import java.util.List;
 
 public record FavoriteFoldersWithFavoriteStatusResponse(
-        List<FavoriteFolderWithFavoriteStatusResponse> favoriteFolders) {
+        List<FavoriteFolderWithFavoriteStatusResponse> favoriteFolders
+) {
 
     public static FavoriteFoldersWithFavoriteStatusResponse from(
             List<FavoriteFolderWithFavoriteStatusResponse> favoriteFolders) {
--- backend/src/main/java/turip/favorite/controller/dto/response/FavoriteFoldersWithPlaceCountResponse.java
+++ backend/src/main/java/turip/favorite/controller/dto/response/FavoriteFoldersWithPlaceCountResponse.java
@@ -2,7 +2,9 @@ package turip.favorite.controller.dto.response;
 
 import java.util.List;
 
-public record FavoriteFoldersWithPlaceCountResponse(List<FavoriteFolderWithPlaceCountResponse> favoriteFolders) {
+public record FavoriteFoldersWithPlaceCountResponse(
+        List<FavoriteFolderWithPlaceCountResponse> favoriteFolders
+) {
 
     public static FavoriteFoldersWithPlaceCountResponse from(
             List<FavoriteFolderWithPlaceCountResponse> favoriteFolders) {
