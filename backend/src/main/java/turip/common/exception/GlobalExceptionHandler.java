abfb3a957b0996114da101172275088ee3407c05
seaniiio <cnc4750@naver.com>
2025-09-15 01:15:32 +0900

    feat: ServletException(NoHandlerFoundException, NoResourceFoundException)에 대한 핸들러 추가

--- backend/src/main/java/turip/common/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/turip/common/exception/GlobalExceptionHandler.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 import turip.common.exception.custom.HttpStatusException;
 import turip.common.exception.custom.IllegalArgumentException;
 
@@ -31,5 +33,19 @@ public class GlobalExceptionHandler {
         log.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ErrorResponse(ErrorTag.INTERNAL_SERVER_ERROR));
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNoHandlerFoundException(NoHandlerFoundException e) {
+        log.warn(e.getMessage(), e);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse(ErrorTag.NOT_FOUND));
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNoResourceFoundException(NoResourceFoundException e) {
+        log.warn(e.getMessage(), e);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse(ErrorTag.NOT_FOUND));
     }
 }
