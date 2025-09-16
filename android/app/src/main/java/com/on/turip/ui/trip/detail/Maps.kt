d7eb97f2570b27f30918ed555155dc15ad96a18a
jerry8282 <ka06196@naver.com>
2025-09-16 12:11:51 +0900

    refactor: Maps enum의 `BASE`네이밍 `DEFAULT`로 변경

--- android/app/src/main/java/com/on/turip/ui/trip/detail/Maps.kt
+++ android/app/src/main/java/com/on/turip/ui/trip/detail/Maps.kt
@@ -9,17 +9,17 @@ private const val KAKAO_BASE_URL = "kakao.com"
 private const val GOOGLE_BASE_URL = "google.com"
 
 enum class Maps(
-    val url: String,
+    val url: String?,
     @DrawableRes val iconRes: Int,
 ) {
     KAKAO(KAKAO_BASE_URL, R.drawable.btn_kakao_map_basic),
     GOOGLE(GOOGLE_BASE_URL, R.drawable.btn_google_map_basic),
-    BASE("", R.drawable.btn_map_link),
+    DEFAULT(null, R.drawable.btn_map_link),
     ;
 
     companion object {
         fun from(mapLink: Uri): Maps =
-            entries.find { mapLink.toString().contains(it.url) }
-                ?: BASE
+            entries.find { mapLink.toString().contains(it.url.orEmpty()) }
+                ?: DEFAULT
     }
 }
