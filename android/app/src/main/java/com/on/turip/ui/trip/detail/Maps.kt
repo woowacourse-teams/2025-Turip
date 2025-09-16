32f8204cef5ce0bc60425baf7d8b057b132b707c
jerry8282 <ka06196@naver.com>
2025-09-16 11:38:22 +0900

    refactor: 맞는 URI가 없을 경우 기본 지도 아이콘을 띄우도록 수정

--- android/app/src/main/java/com/on/turip/ui/trip/detail/Maps.kt
+++ android/app/src/main/java/com/on/turip/ui/trip/detail/Maps.kt
@@ -12,15 +12,14 @@ enum class Maps(
     val url: String,
     @DrawableRes val iconRes: Int,
 ) {
-    KaKao(KAKAO_BASE_URL, R.drawable.ic_kakao_map_basic),
-    Google(GOOGLE_BASE_URL, R.drawable.ic_google_map_basic),
+    KAKAO(KAKAO_BASE_URL, R.drawable.ic_kakao_map_basic),
+    GOOGLE(GOOGLE_BASE_URL, R.drawable.ic_google_map_basic),
+    BASE("", R.drawable.btn_map_link),
     ;
 
     companion object {
-        private const val NOT_FOUND = "%s not found"
-
         fun from(mapLink: Uri): Maps =
             entries.find { mapLink.toString().contains(it.url) }
-                ?: throw IllegalArgumentException(NOT_FOUND.format(mapLink))
+                ?: BASE
     }
 }
