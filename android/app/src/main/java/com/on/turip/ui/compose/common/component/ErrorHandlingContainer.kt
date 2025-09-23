c05a037b98ea206968b5cd09880b90ca6b6f8f8b
yrsel <xxknu88@gmail.com>
2025-09-24 06:25:59 +0900

    refactor: 에러 컨테이너 내부 구현 수정

--- android/app/src/main/java/com/on/turip/ui/compose/common/component/ErrorHandlingContainer.kt
+++ android/app/src/main/java/com/on/turip/ui/compose/common/component/ErrorHandlingContainer.kt
@@ -1,8 +1,6 @@
 package com.on.turip.ui.compose.common.component
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,25 +13,15 @@ fun ErrorHandlingContainer(
     onRetryClick: () -> Unit,
     content: @Composable () -> Unit,
 ) {
-    // 에러가 있는 경우
-    AnimatedVisibility(
-        visible = networkError || serverError,
-        enter = fadeIn(),
-        exit = fadeOut(),
-    ) {
-        ErrorScreen(
-            errorEvent = if (networkError) ErrorEvent.NETWORK_ERROR else ErrorEvent.UNEXPECTED_PROBLEM,
-            onRetryClick = onRetryClick,
-            modifier = Modifier.fillMaxSize(),
-        )
-    }
-
-    // 에러가 없는 경우 일반 콘텐츠
-    AnimatedVisibility(
-        visible = !(networkError || serverError),
-        enter = fadeIn(),
-        exit = fadeOut(),
-    ) {
-        content()
+    Crossfade(targetState = networkError || serverError) { hasError ->
+        if (hasError) {
+            ErrorScreen(
+                errorEvent = if (networkError) ErrorEvent.NETWORK_ERROR else ErrorEvent.UNEXPECTED_PROBLEM,
+                onRetryClick = onRetryClick,
+                modifier = Modifier.fillMaxSize(),
+            )
+        } else {
+            content()
+        }
     }
 }
