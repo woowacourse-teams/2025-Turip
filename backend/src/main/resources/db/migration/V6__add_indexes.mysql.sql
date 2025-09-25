81ff44d777ac13a1eaa44edf0cf0d3af6fed5eda
시원 <cnc4750@inha.edu>
2025-09-26 00:51:59 +0900

    refactor: 인덱스 이름 형식 통일

--- backend/src/main/resources/db/migration/V6__add_indexes.mysql.sql
+++ backend/src/main/resources/db/migration/V6__add_indexes.mysql.sql
@@ -1,4 +1,4 @@
-CREATE INDEX idx_city_name ON city (name);
+CREATE INDEX idx_name ON city (name);
 CREATE INDEX idx_city_id ON content (city_id);
 CREATE INDEX idx_name ON country (name);
 CREATE INDEX idx_country_id ON city (country_id);
