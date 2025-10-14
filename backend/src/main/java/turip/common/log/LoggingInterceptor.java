79825fb1bb15eb15a038ea8a0867a25eebecc9a6
시원 <cnc4750@inha.edu>
2025-10-14 21:51:25 +0900

    refactor: device id 로그를 key값을 device-fid에서 deviceFid로 수정

--- backend/src/main/java/turip/common/log/LoggingInterceptor.java
+++ backend/src/main/java/turip/common/log/LoggingInterceptor.java
@@ -25,7 +25,7 @@ public class LoggingInterceptor implements HandlerInterceptor {
         }
         request.setAttribute(REQUEST_START_TIME_ATTRIBUTE, System.currentTimeMillis());
 
-        MDC.put("device-fid", request.getHeader("device-fid"));
+        MDC.put("deviceFid", request.getHeader("deviceFid"));
         MDC.put("method", request.getMethod());
         MDC.put("uri", request.getRequestURI());
 
@@ -54,7 +54,7 @@ public class LoggingInterceptor implements HandlerInterceptor {
         }
 
         MDC.remove("traceId");
-        MDC.remove("device-fid");
+        MDC.remove("deviceFid");
         MDC.remove("method");
         MDC.remove("uri");
         MDC.remove("httpStatus");
