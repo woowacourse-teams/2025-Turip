7328a7e8c0e657cb96b7de4006f3fe296acf39d7
eunseongu <rndmstjs75@gmail.com>
2025-10-20 17:00:51 +0900

    fix: 카테고리 변환 로직 수정

--- backend/src/main/java/turip/place/controller/dto/response/CategoryResponse.java
+++ backend/src/main/java/turip/place/controller/dto/response/CategoryResponse.java
@@ -2,13 +2,19 @@ package turip.place.controller.dto.response;
 
 import java.util.List;
 import turip.place.domain.Category;
+import turip.place.service.MapProvider;
+import turip.place.service.PlaceCategoryMapper;
 
 public record CategoryResponse(
         String name
 ) {
     public static List<CategoryResponse> from(List<Category> categories) {
         return categories.stream()
-                .map(category -> new CategoryResponse(category.getName()))
+                .map(category -> {
+                    MapProvider provider = MapProvider.getProviderFromCategoryName(category.getName());
+                    String parsedCategoryName = PlaceCategoryMapper.parseCategory(category.getName(), provider);
+                    return new CategoryResponse(parsedCategoryName);
+                })
                 .toList();
     }
 }
