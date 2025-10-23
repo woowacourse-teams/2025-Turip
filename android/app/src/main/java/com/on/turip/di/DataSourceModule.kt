e61b5645ca2ffc4679a266a806cfceb825842e8e
m6z1 <sonmyungj1zz@gmail.com>
2025-10-23 13:31:00 +0900

    feat: DataSource 모듈 구현

--- android/app/src/main/java/com/on/turip/di/DataSourceModule.kt
+++ android/app/src/main/java/com/on/turip/di/DataSourceModule.kt
@@ -4,8 +4,6 @@ import com.on.turip.data.content.datasource.ContentRemoteDataSource
 import com.on.turip.data.content.datasource.DefaultContentRemoteDataSource
 import com.on.turip.data.content.place.datasource.ContentPlaceRemoteDataSource
 import com.on.turip.data.content.place.datasource.DefaultContentPlaceRemoteDataSource
-import com.on.turip.data.creator.datasource.CreatorRemoteDataSource
-import com.on.turip.data.creator.datasource.DefaultCreatorRemoteDataSource
 import com.on.turip.data.favorite.datasource.DefaultFavoriteRemoteDataSource
 import com.on.turip.data.favorite.datasource.FavoriteRemoteDataSource
 import com.on.turip.data.folder.datasource.DefaultFolderRemoteDataSource
@@ -18,33 +16,41 @@ import com.on.turip.data.searchhistory.datasource.DefaultSearchHistoryDataSource
 import com.on.turip.data.searchhistory.datasource.SearchHistoryDataSource
 import com.on.turip.data.userstorage.datasource.DefaultUserStorageLocalDataSource
 import com.on.turip.data.userstorage.datasource.UserStorageLocalDataSource
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 
-object DataSourceModule {
-    val contentRemoteDataSource: ContentRemoteDataSource by lazy {
-        DefaultContentRemoteDataSource(NetworkModule.contentService)
-    }
-    val creatorRemoteDataSource: CreatorRemoteDataSource by lazy {
-        DefaultCreatorRemoteDataSource(NetworkModule.creatorService)
-    }
-    val contentPlaceRemoteDataSource: ContentPlaceRemoteDataSource by lazy {
-        DefaultContentPlaceRemoteDataSource(NetworkModule.contentPlaceService)
-    }
-    val userStorageLocalDataSource: UserStorageLocalDataSource by lazy {
-        DefaultUserStorageLocalDataSource(LocalStorageModule.userStorage)
-    }
-    val favoriteRemoteDataSource: FavoriteRemoteDataSource by lazy {
-        DefaultFavoriteRemoteDataSource(NetworkModule.favoriteService)
-    }
-    val regionRemoteDataSource: RegionRemoteDataSource by lazy {
-        DefaultRegionRemoteDataSource(NetworkModule.regionService)
-    }
-    val searchHistoryDataSource: SearchHistoryDataSource by lazy {
-        DefaultSearchHistoryDataSource(LocalStorageModule.turipDao)
-    }
-    val folderRemoteDataSource: FolderRemoteDataSource by lazy {
-        DefaultFolderRemoteDataSource(NetworkModule.folderService)
-    }
-    val favoritePlaceRemoteDataSource: FavoritePlaceRemoteDataSource by lazy {
-        DefaultFavoritePlaceRemoteDataSource(NetworkModule.favoritePlaceService)
-    }
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataSourceModule {
+    @Binds
+    abstract fun bindContentRemoteDataSource(defaultContentRemoteDataSource: DefaultContentRemoteDataSource): ContentRemoteDataSource
+
+    @Binds
+    abstract fun bindContentPlaceRemoteDataSource(
+        defaultContentPlaceRemoteDataSource: DefaultContentPlaceRemoteDataSource,
+    ): ContentPlaceRemoteDataSource
+
+    @Binds
+    abstract fun bindUserStorageLocalDataSource(
+        defaultUserStorageLocalDataSource: DefaultUserStorageLocalDataSource,
+    ): UserStorageLocalDataSource
+
+    @Binds
+    abstract fun bindFavoriteRemoteDataSource(defaultFavoriteRemoteDataSource: DefaultFavoriteRemoteDataSource): FavoriteRemoteDataSource
+
+    @Binds
+    abstract fun bindRegionRemoteDataSource(defaultRegionRemoteDataSource: DefaultRegionRemoteDataSource): RegionRemoteDataSource
+
+    @Binds
+    abstract fun bindSearchHistoryDataSource(defaultSearchHistoryDataSource: DefaultSearchHistoryDataSource): SearchHistoryDataSource
+
+    @Binds
+    abstract fun bindFolderRemoteDataSource(defaultFolderRemoteDataSource: DefaultFolderRemoteDataSource): FolderRemoteDataSource
+
+    @Binds
+    abstract fun bindFavoritePlaceRemoteDataSource(
+        defaultFavoritePlaceRemoteDataSource: DefaultFavoritePlaceRemoteDataSource,
+    ): FavoritePlaceRemoteDataSource
 }
