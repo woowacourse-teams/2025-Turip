adc472c82da7b2e080797051176c25dc2c49165e
m6z1 <sonmyungj1zz@gmail.com>
2025-10-23 13:31:32 +0900

    feat: LocalStorage 모듈 구현

--- android/app/src/main/java/com/on/turip/di/LocalStorageModule.kt
+++ android/app/src/main/java/com/on/turip/di/LocalStorageModule.kt
@@ -1,17 +1,48 @@
 package com.on.turip.di
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.dataStoreFile
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.room.Room
 import com.on.turip.data.database.TuripDatabase
-import com.on.turip.data.userstorage.DefaultUserStorage
-import com.on.turip.data.userstorage.UserStorage
+import com.on.turip.data.searchhistory.dao.SearchHistoryDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
+@Module
+@InstallIn(SingletonComponent::class)
 object LocalStorageModule {
-    val userStorage: UserStorage by lazy {
-        DefaultUserStorage(ApplicationContextProvider.applicationContext)
-    }
-    private val turipDatabase by lazy {
-        TuripDatabase.getDataBase(ApplicationContextProvider.applicationContext)
-    }
-    val turipDao by lazy {
-        turipDatabase.searchHistoryDao()
-    }
+    private const val USER_PREFERENCES_NAME = "user_storage"
+    private const val TURIP_DATABASE = "turip_database"
+
+    @Provides
+    @Singleton
+    fun provideUserDataStore(
+        @ApplicationContext context: Context,
+    ): DataStore<Preferences> =
+        PreferenceDataStoreFactory.create {
+            context.dataStoreFile(USER_PREFERENCES_NAME)
+        }
+
+    @Provides
+    @Singleton
+    fun provideTuripDatabase(
+        @ApplicationContext context: Context,
+    ): TuripDatabase =
+        Room
+            .databaseBuilder(
+                context,
+                TuripDatabase::class.java,
+                TURIP_DATABASE,
+            ).build()
+
+    @Provides
+    @Singleton
+    fun provideTuripDao(turipDatabase: TuripDatabase): SearchHistoryDao = turipDatabase.searchHistoryDao()
 }
