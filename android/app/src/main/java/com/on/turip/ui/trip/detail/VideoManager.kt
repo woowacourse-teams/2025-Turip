290bb246b6b28a8c0fe2d8828d0939338850d30e
jerry8282 <ka06196@naver.com>
2025-10-30 22:11:10 +0900

    fix: 동영상 재생을 위해 loadDataWithBaseUrl를 통하여 Refer를 주입

--- android/app/src/main/java/com/on/turip/ui/trip/detail/VideoManager.kt
+++ android/app/src/main/java/com/on/turip/ui/trip/detail/VideoManager.kt
@@ -1,6 +1,7 @@
 package com.on.turip.ui.trip.detail
 
 import android.webkit.WebView
+import com.on.turip.BuildConfig
 import com.on.turip.ui.common.TuripUrlConverter
 import com.on.turip.ui.trip.detail.webview.WebViewVideoBridge
 
@@ -24,9 +25,23 @@ class VideoManager(
                 isInitialized = true
             }
 
-            loadUrl(LOAD_URL_FILE_PATH)
+            loadDataWithBaseURL(
+                getBaseUrl(),
+                readLocalHtml(LOCAL_HTML_FILE_NAME),
+                DEFAULT_MIME_TYPE,
+                DEFAULT_ENCODING,
+                null,
+            )
         }
     }
+
+    private fun getBaseUrl(): String = if (BuildConfig.DEBUG) BuildConfig.debug_base_url else BuildConfig.release_base_url
+
+    private fun readLocalHtml(fileName: String): String =
+        webView.context.assets
+            .open(fileName)
+            .bufferedReader()
+            .use { it.readText() }
 
     fun clear() {
         webView.removeJavascriptInterface(BRIDGE_NAME_IN_JS_FILE)
@@ -34,6 +49,8 @@ class VideoManager(
 
     companion object {
         private const val BRIDGE_NAME_IN_JS_FILE: String = "videoBridge"
-        private const val LOAD_URL_FILE_PATH: String = "file:///android_asset/iframe.html"
+        private const val LOCAL_HTML_FILE_NAME: String = "iframe.html"
+        private const val DEFAULT_ENCODING: String = "utf-8"
+        private const val DEFAULT_MIME_TYPE: String = "text/html"
     }
 }
