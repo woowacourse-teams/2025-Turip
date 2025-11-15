21f5aac6bcbad313cb2ffdc0760a9e78ff9f9b2b
jerry8282 <ka06196@naver.com>
2025-11-15 22:58:28 +0900

    feat: 도움말 컴포넌트 추가

---
+++ android/app/src/main/java/com/on/turip/ui/compose/common/component/HelpText.kt
@@ -0,0 +1,53 @@
+package com.on.turip.ui.compose.common.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import com.on.turip.R
+import com.on.turip.ui.compose.theme.TuripTypography
+
+@Composable
+fun HelpText(
+    text: String,
+    style: TextStyle,
+    color: Color,
+    onClickHelp: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            text = text,
+            style = style,
+            color = color,
+            textDecoration = TextDecoration.Underline,
+        )
+        Image(
+            painter = painterResource(R.drawable.btn_help),
+            contentDescription = null,
+            colorFilter = ColorFilter.tint(color),
+            modifier =
+                Modifier.clickable {
+                    onClickHelp
+                },
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun HelpTextPreview() {
+    HelpText("도움말", TuripTypography.titleLarge, Color.White, {})
+}
