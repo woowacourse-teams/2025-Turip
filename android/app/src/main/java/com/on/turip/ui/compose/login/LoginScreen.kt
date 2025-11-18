c96154abf7b50f79c486f54f10bcaa765cc38afa
jerry8282 <ka06196@naver.com>
2025-11-18 15:58:49 +0900

    fix: 로그인 화면의 LoginScreenContent에 상위 modifier를 받지 않도록 수정

--- android/app/src/main/java/com/on/turip/ui/compose/login/LoginScreen.kt
+++ android/app/src/main/java/com/on/turip/ui/compose/login/LoginScreen.kt
@@ -54,7 +54,7 @@ fun LoginScreen(
         )
         LoginScreenContent(
             isHelpTextVisible = isHelpTextVisible,
-            modifier = modifier.padding(innerPadding),
+            modifier = Modifier.padding(innerPadding),
             onClickHelpText = {
                 isHelpTextVisible = true
             },
@@ -117,10 +117,12 @@ private fun LoginScreenContent(
                                         colorResource(R.color.gray_200_c1c1c1),
                                     ),
                                 shape = RoundedCornerShape(10.dp),
-                            ).background(
+                            )
+                            .background(
                                 color = colorResource(R.color.gray_300_5b5b5b),
                                 shape = RoundedCornerShape(10.dp),
-                            ).fillMaxWidth()
+                            )
+                            .fillMaxWidth()
                             .padding(vertical = 20.dp),
                     textAlign = TextAlign.Center,
                 )
