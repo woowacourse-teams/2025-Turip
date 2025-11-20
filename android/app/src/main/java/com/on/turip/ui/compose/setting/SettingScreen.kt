71eb52bf1ff99fb3b31865095113e9de63c9fd7e
yrsel <xxknu88@gmail.com>
2025-11-20 15:56:38 +0900

    feat: 설정 화면 컴포넌트 구현

---
+++ android/app/src/main/java/com/on/turip/ui/compose/setting/SettingScreen.kt
@@ -0,0 +1,91 @@
+package com.on.turip.ui.compose.setting
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.on.turip.R
+import com.on.turip.ui.compose.main.home.component.TuripAppBar
+
+@Composable
+fun SettingScreen(
+    loginStatus: LoginStatus,
+    modifier: Modifier = Modifier,
+) {
+    Scaffold(
+        modifier = modifier,
+        topBar = { TuripAppBar(true) },
+    ) { innerPadding ->
+        Column(
+            modifier =
+                Modifier
+                    .padding(innerPadding)
+                    .fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(5.dp),
+        ) {
+            SettingItem(
+                onClick = {},
+                icon = R.drawable.ic_inquire,
+                title = "문의하기",
+                modifier = Modifier.fillMaxWidth(),
+            )
+            SettingItem(
+                onClick = {},
+                icon = R.drawable.ic_document,
+                title = "개인 정보 처리방침",
+                modifier = Modifier.fillMaxWidth(),
+            )
+            when (loginStatus) {
+                LoginStatus.MEMBER -> {
+                    SettingItem(
+                        onClick = {},
+                        icon = R.drawable.ic_logout,
+                        title = "로그아웃",
+                        modifier = Modifier.fillMaxWidth(),
+                    )
+                    SettingItem(
+                        onClick = {},
+                        icon = R.drawable.ic_withdraw,
+                        title = "회원 탈퇴",
+                        modifier = Modifier.fillMaxWidth(),
+                    )
+                }
+
+                LoginStatus.GUEST -> {
+                    SettingItem(
+                        onClick = {},
+                        icon = R.drawable.ic_login,
+                        title = "로그인",
+                        modifier = Modifier.fillMaxWidth(),
+                    )
+                }
+            }
+        }
+    }
+}
+
+enum class LoginStatus {
+    MEMBER,
+    GUEST,
+}
+
+@Preview(showBackground = true, name = "멤버")
+@Composable
+private fun MemberSettingScreenPreview() {
+    SettingScreen(
+        loginStatus = LoginStatus.MEMBER,
+    )
+}
+
+@Preview(showBackground = true, name = "게스트")
+@Composable
+private fun GuestSettingScreenPreview() {
+    SettingScreen(
+        loginStatus = LoginStatus.GUEST,
+    )
+}
