1abe9e9ac15039da3a19febaca96eaebc2c71881
시원 <cnc4750@inha.edu>
2025-11-21 16:35:34 +0900

    feat: device-fid 헤더로부터 Guest 엔티티를 찾는 argument resolver와 애노테이션 추가

---
+++ backend/src/main/java/turip/auth/resolver/AuthGuestArgumentResolver.java
@@ -0,0 +1,38 @@
+package turip.auth.resolver;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import turip.common.exception.ErrorTag;
+import turip.common.exception.custom.UnauthorizedException;
+import turip.member.service.GuestService;
+
+@Component
+@RequiredArgsConstructor
+public class AuthGuestArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final GuestService guestService;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(AuthGuest.class);
+    }
+
+    @Override
+    public Object resolveArgument(
+            MethodParameter parameter,
+            ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest,
+            WebDataBinderFactory binderFactory
+    ) {
+        String deviceFid = webRequest.getHeader("device-fid");
+        if (deviceFid == null || deviceFid.isBlank()) {
+            throw new UnauthorizedException(ErrorTag.UNAUTHORIZED);
+        }
+        return guestService.findOrCreateByDeviceFid(deviceFid);
+    }
+}
