02ddffea1e3610b6c161dfa2c2e6e9fb5143ef16
시원 <cnc4750@inha.edu>
2025-11-23 18:23:04 +0900

    docs: 회원 탈퇴 api swagger 문서 추가

--- backend/src/main/java/turip/member/controller/MemberController.java
+++ backend/src/main/java/turip/member/controller/MemberController.java
@@ -86,6 +86,56 @@ public class MemberController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(
+            summary = "회원 탈퇴 api",
+            description = "회원을 삭제한다."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "성공 예시"
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "실패 예시",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class),
+                            examples = {
+                                    @ExampleObject(
+                                            name = "access token expired",
+                                            summary = "만료된 access token",
+                                            value = """
+                                                    {
+                                                    	"tag": "ACCESS_TOKEN_EXPIRED",
+                                                    	"message": "access token이 만료됐습니다."
+                                                    }
+                                                    """
+                                    ),
+                                    @ExampleObject(
+                                            name = "invalid signature access token",
+                                            summary = "서명값이 올바르지 않은 access token",
+                                            value = """
+                                                    {
+                                                    	"tag": "ACCESS_TOKEN_SIGNATURE_INVALID",
+                                                    	"message": "access token이 위조됐습니다."
+                                                    }
+                                                    """
+                                    ),
+                                    @ExampleObject(
+                                            name = "unauthorized",
+                                            summary = "알 수 없는 이유로 인증 실패",
+                                            value = """
+                                                    {
+                                                    	"tag": "UNAUTHORIZED",
+                                                    	"message": "토큰 기반 인증에 실패했습니다."
+                                                    }
+                                                    """
+                                    )
+                            }
+                    )
+            )
+    })
     @DeleteMapping("/me")
     public ResponseEntity<Void> delete(@Parameter(hidden = true) @AuthMember Member member) {
         memberService.delete(member);
