84fa83456e30cf9df1a7200062381370691e7fd9
jerry8282 <ka06196@naver.com>
2025-11-24 10:52:44 +0900

    feat: 로컬 스토리지에 토큰 저장 및 조회 기능 추가

--- android/app/src/main/java/com/on/turip/data/userstorage/datasource/UserStorageLocalDataSource.kt
+++ android/app/src/main/java/com/on/turip/data/userstorage/datasource/UserStorageLocalDataSource.kt
@@ -4,4 +4,12 @@ interface UserStorageLocalDataSource {
     suspend fun createId(fid: String): Result<Unit>
 
     suspend fun getId(): Result<String?>
+
+    suspend fun createAccessToken(accessToken: String): Result<Unit>
+
+    suspend fun getAccessToken(accessToken: String): Result<String?>
+
+    suspend fun createRefreshToken(refreshToken: String): Result<Unit>
+
+    suspend fun getRefreshToken(refreshToken: String): Result<String?>
 }
