bb8c7752bfb4727f321d5c9c2d31c629516ceaf2
jerry8282 <ka06196@naver.com>
2025-12-01 18:07:22 +0900

    fix: AuthState 동시성 문제 해결

--- android/app/src/main/java/com/on/turip/common/AuthState.kt
+++ android/app/src/main/java/com/on/turip/common/AuthState.kt
@@ -1,9 +1,11 @@
 package com.on.turip.common
 
 object AuthState {
+    @Volatile
     var type: UserType = UserType.NONE
         private set
 
+    @Synchronized
     fun change(type: UserType) {
         this.type = type
     }
