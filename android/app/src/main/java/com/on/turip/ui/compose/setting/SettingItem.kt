1487bed8ece6f0ea2872475af4420bd55feaaaa5
yrsel <xxknu88@gmail.com>
2025-12-01 15:54:23 +0900

    refactor: SettingItem 아이콘 사이즈 지정

--- android/app/src/main/java/com/on/turip/ui/compose/setting/SettingItem.kt
+++ android/app/src/main/java/com/on/turip/ui/compose/setting/SettingItem.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -33,7 +34,10 @@ fun SettingItem(
         Icon(
             painter = painterResource(uiModel.iconResource),
             contentDescription = null,
-            modifier = Modifier.padding(end = 14.dp),
+            modifier =
+                Modifier
+                    .padding(end = 14.dp)
+                    .size(24.dp),
         )
         Text(
             text = stringResource(uiModel.titleResource),
