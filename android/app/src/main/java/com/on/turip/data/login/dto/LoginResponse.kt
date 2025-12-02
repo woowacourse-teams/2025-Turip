160d2d67963f8a646a738406b05da3ee68aa086c
jerry8282 <ka06196@naver.com>
2025-12-02 17:34:36 +0900

    refactor: 소셜 로그인 로직 관련 sealed LoginResponse 삭제

--- android/app/src/main/java/com/on/turip/data/login/dto/LoginResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.on.turip.data.login.dto
-
-import androidx.credentials.GetCredentialResponse
-
-sealed interface LoginResponse {
-    data class GoogleLogin(
-        val getCredentialResponse: GetCredentialResponse,
-    ) : LoginResponse
-}
