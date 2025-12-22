1566c738311cf8c3e72b24787e6250e4c9914315
yrsel <xxknu88@gmail.com>
2025-12-22 23:11:32 +0900

    refactor: safeApiCall 재설계
    - 상태코드 204 인 경우, 모든 api가 Unit 반환하고 있기에 body가 비어있는 경우 존재하지 않아 코드 수정
    - response 호출 실패한 경우 HttpError 반환
    - 코루틴 예외는 구조화된 동시성을 깨뜨리지 않도록 그대로 예외 전달
    - 네트워크 에러인 경우 NetworkError 반환 (이 부분은 시스템 네트워크 상태 관찰하고 판단하는 구조로 추가 보완 필요)
    - 그 외 예외인 경우 UnknownError 반환

--- android/app/src/main/java/com/on/turip/data/common/SafeApiCall.kt
+++ android/app/src/main/java/com/on/turip/data/common/SafeApiCall.kt
@@ -1,31 +1,29 @@
 package com.on.turip.data.common
 
-import retrofit2.HttpException
+import com.on.turip.data.common.data.ErrorResponse
+import com.on.turip.data.common.data.toErrorType
+import com.on.turip.data.common.domain.ErrorType
+import kotlinx.coroutines.CancellationException
+import kotlinx.serialization.json.Json
 import retrofit2.Response
+import java.io.IOException
 
-@Suppress("UNCHECKED_CAST")
-suspend inline fun <T> safeApiCall(apiCall: suspend () -> Response<T>): TuripCustomResult<T> =
+suspend fun <T> safeApiCall(apiCall: suspend () -> Response<T>): TuripCustomResult<T> {
     try {
         val response: Response<T> = apiCall()
-
         if (response.isSuccessful) {
-            val body = response.body()
-            if (body != null) {
-                TuripCustomResult.Success(body)
-            } else {
-                if (response.code() == 204) {
-                    TuripCustomResult.Success(Unit as T)
-                } else {
-                    TuripCustomResult.ParseError(
-                        IllegalStateException("예상치 못한 빈 응답입니다."),
-                    )
-                }
-            }
-        } else {
-            TuripCustomResult.HttpError(response.code())
+            @Suppress("UNCHECKED_CAST")
+            return TuripCustomResult.Success(response.body() as T)
         }
-    } catch (e: HttpException) {
-        TuripCustomResult.HttpError(e.code())
-    } catch (e: Exception) {
-        TuripCustomResult.NetworkError(e)
+
+        val errorResponse: ErrorResponse? =
+            response.errorBody()?.string()?.let { Json.decodeFromString<ErrorResponse>(it) }
+        return TuripCustomResult.Failure(errorResponse.toErrorType())
+    } catch (e: Throwable) {
+        if (e is CancellationException) throw e
+        return when (e) {
+            is IOException -> TuripCustomResult.Failure(ErrorType.Network)
+            else -> TuripCustomResult.Failure(ErrorType.Unknown)
+        }
     }
+}
