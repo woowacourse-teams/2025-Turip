6dd8f5b946082917d9b788bf2f12eac3198b88e6
yrsel <xxknu88@gmail.com>
2025-12-30 00:03:17 +0900

    refactor: HomeScreen 로딩 추가 및 상태 기반으로 화면 분기하며 flow 로 전환

--- android/app/src/main/java/com/on/turip/ui/compose/home/HomeScreen.kt
+++ android/app/src/main/java/com/on/turip/ui/compose/home/HomeScreen.kt
@@ -2,26 +2,28 @@ package com.on.turip.ui.compose.home
 
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
@@ -31,7 +33,6 @@ import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.on.turip.R
 import com.on.turip.data.common.ErrorUiState
-import com.on.turip.domain.region.RegionCategory
 import com.on.turip.ui.common.event.CommonUiEffect
 import com.on.turip.ui.compose.designsystem.component.ErrorScreen
 import com.on.turip.ui.compose.designsystem.component.TuripAppBar
@@ -42,6 +43,7 @@ import com.on.turip.ui.compose.home.component.UsersLikeList
 import com.on.turip.ui.compose.theme.TuripTheme
 import com.on.turip.ui.compose.theme.TuripTypography
 import com.on.turip.ui.main.home.model.UsersLikeContentModel
+import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 fun HomeScreen(
@@ -51,124 +53,213 @@ fun HomeScreen(
     navigateToLoginScreen: () -> Unit,
     viewModel: HomeViewModel = hiltViewModel(),
 ) {
-    var keyword: String by rememberSaveable { mutableStateOf("") }
-
-    val regions: List<RegionCategory> by viewModel.regionCategories.observeAsState(emptyList())
-    val isSelectedDomestic: Boolean by viewModel.isSelectedDomestic.observeAsState(true)
-    val usersLikeContents: List<UsersLikeContentModel> by viewModel.usersLikeContents.observeAsState(
-        emptyList(),
-    )
-    val errorUiState: ErrorUiState by viewModel.errorUiState.collectAsStateWithLifecycle()
-    val scrollState = rememberScrollState()
-    val focusManager = LocalFocusManager.current
-    val keyboardController = LocalSoftwareKeyboardController.current
+    val uiState: HomeUiState by viewModel.uiState.collectAsStateWithLifecycle()
 
     LaunchedEffect(Unit) {
-        viewModel.commonUiEffect.collect { uiEffect: CommonUiEffect ->
+        viewModel.commonUiEffect.collectLatest { uiEffect: CommonUiEffect ->
             when (uiEffect) {
                 CommonUiEffect.NavigateToLogin -> navigateToLoginScreen()
             }
         }
     }
 
+    HomeScreenLayout(
+        uiState = uiState,
+        onRetryLoadContents = viewModel::loadContents,
+        onSearchClick = onSearchClick,
+        onContentClick = onContentClick,
+        onRegionClick = onRegionClick,
+        onDomesticClick = { viewModel.updateDomesticSelected(it) },
+    )
+}
+
+@Composable
+private fun HomeScreenLayout(
+    uiState: HomeUiState,
+    onRetryLoadContents: () -> Unit,
+    onSearchClick: (String) -> Unit,
+    onContentClick: (UsersLikeContentModel) -> Unit,
+    onRegionClick: (String) -> Unit,
+    onDomesticClick: (Boolean) -> Unit,
+) {
     Scaffold(
         topBar = {
-            TuripAppBar(
-                canBack = false,
-            )
+            TuripAppBar(canBack = false)
         },
-        modifier =
-            Modifier.pointerInput(Unit) {
-                detectTapGestures(onTap = {
-                    focusManager.clearFocus()
-                    keyboardController?.hide()
-                })
-            },
     ) { innerPadding ->
+        HomeScreenContent(
+            uiState = uiState,
+            onSearchClick = onSearchClick,
+            onRetryLoadContents = onRetryLoadContents,
+            onContentClick = onContentClick,
+            onRegionClick = onRegionClick,
+            onDomesticClick = onDomesticClick,
+            modifier = Modifier.padding(innerPadding),
+        )
+    }
+}
 
-        when (val state: ErrorUiState = errorUiState) {
-            ErrorUiState.Server, ErrorUiState.Network -> {
-                ErrorScreen(
-                    errorUiState = state,
-                    onRetryClick = { viewModel.reload() },
-                    modifier =
-                        Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding),
-                )
+@Composable
+private fun HomeScreenContent(
+    uiState: HomeUiState,
+    onRetryLoadContents: () -> Unit,
+    onSearchClick: (String) -> Unit,
+    onContentClick: (UsersLikeContentModel) -> Unit,
+    onRegionClick: (String) -> Unit,
+    onDomesticClick: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    var keyword: String by rememberSaveable { mutableStateOf("") }
+    val scrollState = rememberScrollState()
+    val focusManager = LocalFocusManager.current
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    when {
+        uiState.errorUiState != ErrorUiState.None -> {
+            ErrorScreen(
+                errorUiState = uiState.errorUiState,
+                onRetryClick = onRetryLoadContents,
+                modifier = modifier.fillMaxSize(),
+            )
+        }
+
+        uiState.isLoading -> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center,
+            ) {
+                CircularProgressIndicator(modifier = Modifier.size(60.dp), color = Color.Black)
             }
+        }
 
-            ErrorUiState.None -> {
-                Column(
+        else -> {
+            Column(
+                modifier =
+                    modifier
+                        .fillMaxSize()
+                        .pointerInput(Unit) {
+                            detectTapGestures(onTap = {
+                                focusManager.clearFocus()
+                                keyboardController?.hide()
+                            })
+                        }.padding(horizontal = 20.dp)
+                        .verticalScroll(scrollState),
+                verticalArrangement = Arrangement.spacedBy(10.dp),
+            ) {
+                Text(
+                    text = stringResource(R.string.home_top_title),
+                    color = colorResource(R.color.gray_400_2b2b2b),
+                    style = TuripTypography.titleLarge,
+                )
+
+                SearchTextField(
+                    keyword = keyword,
+                    onKeywordChange = { newKeyword -> keyword = newKeyword },
+                    onSearch = { keyword ->
+                        onSearchClick(keyword)
+                        focusManager.clearFocus(force = true)
+                        keyboardController?.hide()
+                    },
                     modifier =
                         Modifier
-                            .fillMaxSize()
-                            .padding(
-                                top = innerPadding.calculateTopPadding(),
-                                start = 20.dp,
-                                end = innerPadding.calculateEndPadding(LocalLayoutDirection.current),
-                            ).verticalScroll(scrollState),
-                    verticalArrangement = Arrangement.spacedBy(10.dp),
-                ) {
-                    Text(
-                        text = stringResource(R.string.home_top_title),
-                        color = colorResource(R.color.gray_400_2b2b2b),
-                        style = TuripTypography.titleLarge,
-                    )
+                            .wrapContentSize()
+                            .padding(top = 4.dp, end = 20.dp),
+                )
 
-                    SearchTextField(
-                        keyword = keyword,
-                        onKeywordChange = { newKeyword -> keyword = newKeyword },
-                        onSearch = { keyword ->
-                            onSearchClick(keyword)
-                            focusManager.clearFocus(force = true)
-                            keyboardController?.hide()
-                        },
-                        modifier =
-                            Modifier
-                                .wrapContentSize()
-                                .padding(top = 4.dp, end = 20.dp),
-                    )
+                Text(
+                    text = stringResource(R.string.home_users_like_content_title),
+                    modifier = Modifier.padding(top = 14.dp),
+                    color = colorResource(R.color.gray_400_2b2b2b),
+                    style = TuripTypography.titleLarge,
+                )
 
-                    Text(
-                        text = stringResource(R.string.home_users_like_content_title),
-                        modifier = Modifier.padding(top = 14.dp),
-                        color = colorResource(R.color.gray_400_2b2b2b),
-                        style = TuripTypography.titleLarge,
-                    )
+                UsersLikeList(
+                    usersLikeContents = uiState.usersLikeContents,
+                    onContentClick = onContentClick,
+                )
 
-                    UsersLikeList(
-                        usersLikeContents = usersLikeContents,
-                        onContentClick = onContentClick,
-                    )
+                RegionTypeButtons(
+                    onDomesticClick = { onDomesticClick(it) },
+                    isSelectedDomestic = uiState.isDomesticSelected,
+                )
 
-                    RegionTypeButtons(
-                        onDomesticClick = { isSelectDomestic: Boolean ->
-                            viewModel.updateDomesticSelected(isSelectDomestic)
-                        },
-                        isSelectedDomestic = isSelectedDomestic,
-                    )
-
-                    RegionList(
-                        regions = regions,
-                        onRegionClick = onRegionClick,
-                        modifier = Modifier.padding(end = 20.dp),
-                    )
-                }
+                RegionList(
+                    regions = uiState.regionCategories,
+                    onRegionClick = onRegionClick,
+                    modifier = Modifier.padding(end = 20.dp),
+                )
             }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, name = "로딩")
 @Composable
-fun HomeScreenPreview() {
+fun Home_Loading_Preview() {
+    val uiState = HomeUiState.Idle
     TuripTheme {
-        HomeScreen(
+        HomeScreenLayout(
+            uiState = uiState.copy(isLoading = true, errorUiState = ErrorUiState.None),
+            onRetryLoadContents = {},
             onSearchClick = {},
-            onRegionClick = {},
             onContentClick = {},
-            navigateToLoginScreen = {},
+            onRegionClick = {},
+            onDomesticClick = {},
+        )
+    }
+}
+
+@Preview(showBackground = true, name = "성공")
+@Composable
+fun Home_Success_Preview() {
+    val uiState =
+        HomeUiState(
+            isLoading = false,
+            regionCategories = emptyList(),
+            isDomesticSelected = true,
+            usersLikeContents = emptyList(),
+            errorUiState = ErrorUiState.None,
+        )
+    TuripTheme {
+        HomeScreenLayout(
+            uiState = uiState,
+            onRetryLoadContents = {},
+            onSearchClick = {},
+            onContentClick = {},
+            onRegionClick = {},
+            onDomesticClick = {},
+        )
+    }
+}
+
+@Preview(showBackground = true, name = "서버 에러 발생")
+@Composable
+fun Home_Server_Error_Preview() {
+    val uiState = HomeUiState.Idle
+    TuripTheme {
+        HomeScreenLayout(
+            uiState = uiState.copy(isLoading = false, errorUiState = ErrorUiState.Server),
+            onRetryLoadContents = {},
+            onSearchClick = {},
+            onContentClick = {},
+            onRegionClick = {},
+            onDomesticClick = {},
+        )
+    }
+}
+
+@Preview(showBackground = true, name = "네트워크 에러 발생")
+@Composable
+fun Home_Network_Error_Preview() {
+    val uiState = HomeUiState.Idle
+    TuripTheme {
+        HomeScreenLayout(
+            uiState = uiState.copy(isLoading = false, errorUiState = ErrorUiState.Network),
+            onRetryLoadContents = {},
+            onSearchClick = {},
+            onContentClick = {},
+            onRegionClick = {},
+            onDomesticClick = {},
         )
     }
 }
--- android/app/src/main/java/com/on/turip/ui/compose/home/HomeViewModel.kt
+++ android/app/src/main/java/com/on/turip/ui/compose/home/HomeViewModel.kt
@@ -1,11 +1,10 @@
 package com.on.turip.ui.compose.home
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.on.turip.data.common.ErrorType
 import com.on.turip.data.common.ErrorUiState
+import com.on.turip.data.common.TuripCustomResult
 import com.on.turip.data.common.UiError
 import com.on.turip.data.common.onFailure
 import com.on.turip.data.common.onFailureWithCause
@@ -17,8 +16,8 @@ import com.on.turip.domain.region.RegionCategory
 import com.on.turip.domain.region.repository.RegionRepository
 import com.on.turip.ui.common.event.CommonUiEffect
 import com.on.turip.ui.common.mapper.toUiModel
-import com.on.turip.ui.main.home.model.UsersLikeContentModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -35,59 +34,77 @@ class HomeViewModel @Inject constructor(
     private val regionRepository: RegionRepository,
     private val contentRepository: ContentRepository,
 ) : ViewModel() {
-    private val _regionCategories: MutableLiveData<List<RegionCategory>> = MutableLiveData()
-    val regionCategories: LiveData<List<RegionCategory>> get() = _regionCategories
-
-    private val _isSelectedDomestic: MutableLiveData<Boolean> = MutableLiveData()
-    val isSelectedDomestic: LiveData<Boolean> get() = _isSelectedDomestic
-
-    private val _usersLikeContents: MutableLiveData<List<UsersLikeContentModel>> =
-        MutableLiveData()
-    val usersLikeContents: LiveData<List<UsersLikeContentModel>> get() = _usersLikeContents
-
-    private val _errorUiState: MutableStateFlow<ErrorUiState> = MutableStateFlow(ErrorUiState.None)
-    val errorUiState: StateFlow<ErrorUiState> = _errorUiState.asStateFlow()
+    private val _uiState: MutableStateFlow<HomeUiState> = MutableStateFlow(HomeUiState.Idle)
+    val uiState: StateFlow<HomeUiState> = _uiState.asStateFlow()
 
     private val _commonUiEffect: Channel<CommonUiEffect> = Channel(Channel.BUFFERED)
     val commonUiEffect: Flow<CommonUiEffect> = _commonUiEffect.receiveAsFlow()
 
     init {
-        loadUsersLikeContents()
-        loadRegionCategories(isDomestic = true)
+        loadContents()
     }
 
-    fun reload() {
-        loadUsersLikeContents()
-        loadRegionCategories(isDomestic = true)
-    }
-
-    private fun loadUsersLikeContents() {
+    fun loadContents() {
         viewModelScope.launch {
-            contentRepository
-                .loadPopularFavoriteContents()
-                .onSuccess { contents: List<UsersLikeContent> ->
-                    _usersLikeContents.value = contents.map { it.toUiModel() }
-                    _errorUiState.update { ErrorUiState.None }
-                    Timber.d("인기 찜 목록: $contents")
-                }.onFailure { errorType: ErrorType ->
-                    when (val uiError: UiError = errorType.toUiError()) {
-                        is UiError.Global -> handleGlobalError(uiError)
-                        is UiError.Feature -> Unit
-                    }
-                }.onFailureWithCause { errorType: ErrorType, cause: Throwable? ->
-                    Timber.e("인기 찜 목록 불러오기 실패 : $errorType / $cause")
-                }
+            _uiState.update { it.copy(isLoading = true, errorUiState = ErrorUiState.None) }
+            val usersLikeContentsDeferred =
+                async { contentRepository.loadPopularFavoriteContents() }
+            val regionCategoriesDeferred =
+                async { regionRepository.loadRegionCategories(uiState.value.isDomesticSelected) }
+
+            val usersLikeContentsResult = usersLikeContentsDeferred.await()
+            val regionCategoriesResult = regionCategoriesDeferred.await()
+
+            val failure: TuripCustomResult.Failure? =
+                listOf(usersLikeContentsResult, regionCategoriesResult)
+                    .filterIsInstance<TuripCustomResult.Failure>()
+                    .firstOrNull()
+
+            if (failure != null) {
+                handleGlobalError(failure.errorType.toUiError())
+                return@launch
+            }
+
+            val usersLikeContents: List<UsersLikeContent> =
+                (usersLikeContentsResult as TuripCustomResult.Success).value
+            val regionCategories: List<RegionCategory> =
+                (regionCategoriesResult as TuripCustomResult.Success).value
+
+            _uiState.update { state: HomeUiState ->
+                state.copy(
+                    isLoading = false,
+                    regionCategories = regionCategories,
+                    usersLikeContents = usersLikeContents.map { it.toUiModel() },
+                    errorUiState = ErrorUiState.None,
+                )
+            }
+
+            Timber.d("인기 찜 목록: $usersLikeContents")
+            Timber.d("지역 카테고리 조회: $regionCategories")
         }
+    }
+
+    fun updateDomesticSelected(isDomesticSelected: Boolean) {
+        _uiState.update { it.copy(isDomesticSelected = isDomesticSelected) }
+        Timber.d(if (isDomesticSelected) "국내 클릭" else "해외 클릭")
+        loadRegionCategories(isDomesticSelected)
     }
 
     private fun loadRegionCategories(isDomestic: Boolean) {
         viewModelScope.launch {
+            _uiState.update { it.copy(isLoading = true, errorUiState = ErrorUiState.None) }
+
             regionRepository
                 .loadRegionCategories(isDomestic)
                 .onSuccess { regionCategories: List<RegionCategory> ->
-                    _regionCategories.value = regionCategories
-                    _isSelectedDomestic.value = isDomestic
-                    _errorUiState.update { ErrorUiState.None }
+                    _uiState.update { state: HomeUiState ->
+                        state.copy(
+                            isLoading = false,
+                            regionCategories = regionCategories,
+                            isDomesticSelected = isDomestic,
+                            errorUiState = ErrorUiState.None,
+                        )
+                    }
                     Timber.d("지역 카테고리 조회: $regionCategories")
                 }.onFailure { errorType: ErrorType ->
                     when (val uiError: UiError = errorType.toUiError()) {
@@ -100,17 +117,25 @@ class HomeViewModel @Inject constructor(
         }
     }
 
-    fun updateDomesticSelected(isDomesticSelected: Boolean) {
-        _isSelectedDomestic.value = isDomesticSelected
-        Timber.d(if (isSelectedDomestic.value == true) "국내 클릭" else "해외 클릭")
-        isSelectedDomestic.value?.let { loadRegionCategories(it) }
-    }
+    private suspend fun handleGlobalError(uiError: UiError) {
+        if (uiError is UiError.Global) {
+            when (uiError) {
+                UiError.Global.Network -> {
+                    _uiState.update {
+                        it.copy(isLoading = false, errorUiState = ErrorUiState.Network)
+                    }
+                }
 
-    private suspend fun handleGlobalError(uiError: UiError.Global) {
-        when (uiError) {
-            UiError.Global.Network -> _errorUiState.update { ErrorUiState.Network }
-            UiError.Global.Server -> _errorUiState.update { ErrorUiState.Server }
-            UiError.Global.TokenExpired -> _commonUiEffect.send(CommonUiEffect.NavigateToLogin)
+                UiError.Global.Server -> {
+                    _uiState.update {
+                        it.copy(isLoading = false, errorUiState = ErrorUiState.Server)
+                    }
+                }
+
+                UiError.Global.TokenExpired -> {
+                    _commonUiEffect.send(CommonUiEffect.NavigateToLogin)
+                }
+            }
         }
     }
 }
