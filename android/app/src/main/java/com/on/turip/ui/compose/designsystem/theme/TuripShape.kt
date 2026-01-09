613f4756872c8250dc62ce52dbeba93bc55c9294
jerry8282 <ka06196@naver.com>
2026-01-09 16:52:43 +0900

    feat: TuripShape에 largeContainer 속성 추가 (16.dp)

--- android/app/src/main/java/com/on/turip/ui/compose/designsystem/theme/TuripShape.kt
+++ android/app/src/main/java/com/on/turip/ui/compose/designsystem/theme/TuripShape.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.unit.dp
 @Immutable
 data class TuripShape(
     val container: Shape,
+    val largeContainer: Shape,
     val chip: Shape,
     val bottomSheetRounded: Shape,
     val wideButton: Shape,
@@ -26,6 +27,7 @@ data class TuripShape(
 internal val Shape =
     TuripShape(
         container = RoundedCornerShape(8.dp),
+        largeContainer = RoundedCornerShape(16.dp),
         chip = RoundedCornerShape(12.dp),
         bottomSheetRounded = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
         wideButton = RoundedCornerShape(24.dp),
@@ -35,6 +37,7 @@ val LocalShape =
     staticCompositionLocalOf {
         TuripShape(
             container = RoundedCornerShape(0.dp),
+            largeContainer = RoundedCornerShape(0.dp),
             chip = RoundedCornerShape(0.dp),
             bottomSheetRounded = RoundedCornerShape(0.dp),
             wideButton = RoundedCornerShape(0.dp),
