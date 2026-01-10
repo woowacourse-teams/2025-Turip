3cf1946787dda992ef4bb41209c1adb2f1f5f1dd
jerry8282 <ka06196@naver.com>
2026-01-11 05:57:29 +0900

    refactor: ktlint 적용

--- android/app/src/main/java/com/on/turip/ui/compose/designsystem/component/TuripDialog.kt
+++ android/app/src/main/java/com/on/turip/ui/compose/designsystem/component/TuripDialog.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import com.on.turip.ui.compose.designsystem.theme.TuripTheme
