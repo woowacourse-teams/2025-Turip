4deb5534f31e034803f0665143bd2e2a7443b50f
jerry8282 <ka06196@naver.com>
2026-01-11 05:25:45 +0900

    refactor: HelpText 컴포넌트 간격 padding -> width

--- android/app/src/main/java/com/on/turip/ui/compose/login/component/HelpText.kt
+++ android/app/src/main/java/com/on/turip/ui/compose/login/component/HelpText.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -41,7 +42,7 @@ fun HelpText(
                     onClickText()
                 },
         )
-        Spacer(modifier = Modifier.padding(TuripTheme.spacing.extraSmall))
+        Spacer(modifier = Modifier.width(TuripTheme.spacing.small))
         Image(
             painter = painterResource(R.drawable.btn_help),
             contentDescription = null,
