d814791bc16bc8c20fab20b7bf02f70446547068
시원 <cnc4750@inha.edu>
2026-01-11 15:40:32 +0100

    feat: 회원가입 api에 대한 swagger 문서 추가

--- backend/src/main/java/turip/account/controller/TuripMemberController.java
+++ backend/src/main/java/turip/account/controller/TuripMemberController.java
@@ -1,5 +1,11 @@
 package turip.account.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -11,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 import turip.account.controller.dto.request.TuripMemberRequest;
 import turip.account.controller.dto.response.TuripMemberResponse;
 import turip.account.service.TuripMemberService;
+import turip.auth.controller.dto.response.LoginResponse;
+import turip.common.exception.ErrorResponse;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +28,89 @@ public class TuripMemberController {
 
     private final TuripMemberService turipMemberService;
 
+    @Operation(
+            summary = "자체 회원가입 api",
+            description = "튜립 자체 회원가입을 진행한다."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "성공 예시",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = LoginResponse.class),
+                            examples = @ExampleObject(
+                                    name = "success",
+                                    summary = "로그인 성공",
+                                    value = """
+                                            {
+                                                "id": 1,
+                                                "memberId": 15,
+                                                "loginId": "turip"
+                                            }
+                                            """
+                            )
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "실패 예시",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class),
+                            examples = {
+                                    @ExampleObject(
+                                            name = "invalid login_password",
+                                            summary = "올바르지 않은 비밀번호 형식",
+                                            value = """
+                                                    {
+                                                    	"tag": "LOGIN_PASSWORD_INVALID",
+                                                    	"message": "비밀번호 형식이 올바르지 않습니다."
+                                                    }
+                                                    """
+                                    ),
+                                    @ExampleObject(
+                                            name = "invalid login_id",
+                                            summary = "올바르지 않은 아이디 형식",
+                                            value = """
+                                                    {
+                                                    	"tag": "LOGIN_ID_INVALID",
+                                                    	"message": "아이디 형식이 올바르지 않습니다."
+                                                    }
+                                                    """
+                                    ),
+                                    @ExampleObject(
+                                            name = "invalid email",
+                                            summary = "올바르지 않은 이메일 형식",
+                                            value = """
+                                                    {
+                                                    	"tag": "EMAIL_INVALID",
+                                                    	"message": "이메일 형식이 올바르지 않습니다."
+                                                    }
+                                                    """
+                                    )
+                            }
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "실패 예시",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class),
+                            examples = @ExampleObject(
+                                    name = "login_id conflict",
+                                    summary = "아이디 중복",
+                                    value = """
+                                            {
+                                            	"tag": "LOGIN_ID_CONFLICT",
+                                            	"message": "중복 아이디가 존재합니다."
+                                            }
+                                            """
+                            )
+                    )
+            )
+    })
     @PostMapping
     public ResponseEntity<TuripMemberResponse> create(@RequestBody TuripMemberRequest request) {
         TuripMemberResponse response = turipMemberService.create(request);
