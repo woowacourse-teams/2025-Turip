b3d41062c0f1da52dd344e96944296c357ead102
yrsel <xxknu88@gmail.com>
2026-01-17 14:47:17 +0900

    refactor: WebView 비디오 재개 Util 함수 추가
    - Compose에서는 ViewSystem과 다르게 전체화면으로 전환할 때 콜백을 통해 화면을 만들 수 밖에 없어, 비동기적으로 화면이 그려지는 과정이 발생
    이 과정에서 YoutubePlayer는 화면이 벗어났다고 판단해 영상을 정지하는 문제가 발생
    그러므로 재생 중이거나, 버퍼링 상태일 때는 화면 전환 후에 자동으로 재생하도록 JS 코드로 제어

--- android/app/src/main/java/com/on/turip/ui/compose/trip/webview/WebViewUtils.kt
+++ android/app/src/main/java/com/on/turip/ui/compose/trip/webview/WebViewUtils.kt
@@ -23,3 +23,23 @@ fun WebView.navigateToTimeLine(seconds: Int) {
         null,
     )
 }
+
+/**
+ * 재생 중이거나 버퍼링이라면 재생을 재개한다.
+ */
+fun WebView.resumeVideo() {
+    // Youtube Player API state
+    // -1 : 시작 되지 않음, 0 : 재생 완료, 1 : 재생 중, 2 : 일시 중지, 3 : 버퍼링
+    val script =
+        """
+        (function() {
+            if (typeof player !== 'undefined' && player.getPlayerState) {
+                var state = player.getPlayerState();
+                if (state === 1 || state === 3) {
+                    player.playVideo();
+                }
+            }
+        })();
+        """.trimIndent()
+    evaluateJavascript(script, null)
+}
