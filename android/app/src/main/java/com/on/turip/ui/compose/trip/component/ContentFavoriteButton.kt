c70bf0e448e4ef2ba9797cca6467c1af23a03f1f
yrsel <xxknu88@gmail.com>
2026-01-19 11:55:58 +0900

    refactor: 컨텐츠 찜 버튼 컴포넌트 리플 효과 영역 수정

--- android/app/src/main/java/com/on/turip/ui/compose/trip/component/ContentFavoriteButton.kt
+++ android/app/src/main/java/com/on/turip/ui/compose/trip/component/ContentFavoriteButton.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,6 +39,7 @@ fun ContentFavoriteButton(
         verticalAlignment = Alignment.CenterVertically,
         modifier =
             modifier
+                .clip(TuripTheme.shape.wideButton)
                 .clickable(onClick = onClick)
                 .background(color = TuripTheme.colors.primary, shape = TuripTheme.shape.wideButton)
                 .fillMaxWidth()
