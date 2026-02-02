d347ba6877ea4548817f543ec7e8d6e42294b9e7
yrsel <xxknu88@gmail.com>
2026-02-02 13:17:35 +0900

    refactor: 추상화 제거 및 util 객체로 데이터 생성

--- android/app/src/main/java/com/on/turip/ui/compose/setting/model/AppEnvironmentInfoModel.kt
+++ android/app/src/main/java/com/on/turip/ui/compose/setting/model/AppEnvironmentInfoModel.kt
@@ -1,6 +1,6 @@
-package com.on.turip.domain.common
+package com.on.turip.ui.compose.setting.model
 
-data class AppEnvironmentInfo(
+data class AppEnvironmentInfoModel(
     val appVersionName: String,
     val appVersionCode: Int,
     val deviceReleaseVersion: String,
