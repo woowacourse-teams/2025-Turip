da2c929ccb6b0198227e3272f5a648ee34569fc9
시원 <cnc4750@inha.edu>
2026-02-03 12:04:16 +0900

    refactor: 사용하지 않는 메서드 제거

--- backend/turip-app/src/main/java/turip/favorite/repository/FavoriteFolderRepository.java
+++ backend/turip-app/src/main/java/turip/favorite/repository/FavoriteFolderRepository.java
@@ -1,6 +1,5 @@
 package turip.favorite.repository;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import turip.account.domain.Account;
 import turip.favorite.domain.FavoriteFolder;
@@ -8,7 +7,5 @@ import turip.favorite.domain.FavoriteFolder;
 public interface FavoriteFolderRepository extends JpaRepository<FavoriteFolder, Long> {
 
     // TODO: account 관련 수정하기
-    List<FavoriteFolder> findAllByAccount(Account account);
-
     boolean existsByAccountAndIsDefault(Account account, boolean isDefault);
 }
