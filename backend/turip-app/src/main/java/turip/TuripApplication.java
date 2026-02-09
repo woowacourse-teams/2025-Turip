2957a4cf3d38cde2d1f34b84ba25b0845bfd9a06
시원 <cnc4750@inha.edu>
2026-02-09 14:52:30 +0900

    fix: `@EnableRetry` 애노테이션 제거

--- backend/turip-app/src/main/java/turip/TuripApplication.java
+++ backend/turip-app/src/main/java/turip/TuripApplication.java
@@ -2,9 +2,7 @@ package turip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.retry.annotation.EnableRetry;
 
-@EnableRetry
 @SpringBootApplication
 public class TuripApplication {
 
