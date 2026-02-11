87ee0978cfeb63b17c17c950657e45c480999502
시원 <cnc4750@inha.edu>
2026-02-11 20:19:58 +0900

    refactor: json 응답 명세에 맞게 수정

--- backend/turip-app/src/main/java/turip/favorite/stream/controller/dto/response/ConnectStreamResponse.java
+++ backend/turip-app/src/main/java/turip/favorite/stream/controller/dto/response/ConnectStreamResponse.java
@@ -3,7 +3,7 @@ package turip.favorite.stream.controller.dto.response;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDateTime;
 
-public record ConnectStreamResponse(@JsonProperty("folderId") Long favoriteFolderId, LocalDateTime timestamp) {
+public record ConnectStreamResponse(@JsonProperty("turipId") Long favoriteFolderId, LocalDateTime timestamp) {
 
     public static ConnectStreamResponse from(Long favoriteFolderId) {
         return new ConnectStreamResponse(favoriteFolderId, LocalDateTime.now());
