26f676b2ef2e040f37751cab1d9a59a639f68c76
eunseongu <rndmstjs75@gmail.com>
2026-02-17 03:15:32 +0900

    feat: 패키지 이동 및 타입 추가

--- backend/turip-app/src/main/java/turip/favorite/domain/event/ActionType.java
+++ backend/turip-app/src/main/java/turip/favorite/domain/event/ActionType.java
@@ -1,11 +1,16 @@
-package turip.favorite.stream.service;
+package turip.favorite.domain.event;
 
 public enum ActionType {
     // FavoritePlace 관련
     PLACE_REORDERED,
     PLACE_ADDED,
     PLACE_DELETED,
-    NAME_CHANGED,
+
+    // FavoriteFolder 관련
+    FOLDER_NAME_CHANGED,
+    FOLDER_DELETED,
+    FOLDER_PLACE_CHANGED,
+
     // Member 관련
     MEMBER_JOINED,
     MEMBER_EXITED,
