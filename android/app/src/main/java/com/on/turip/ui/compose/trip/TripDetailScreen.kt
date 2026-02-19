bde8414fe067af96b732ad9521401ac519f519e9
yrsel <xxknu88@gmail.com>
2026-02-19 13:46:02 +0900

    refactor: 콘텐츠 장소 item key 값 수정

--- android/app/src/main/java/com/on/turip/ui/compose/trip/TripDetailScreen.kt
+++ android/app/src/main/java/com/on/turip/ui/compose/trip/TripDetailScreen.kt
@@ -333,7 +333,7 @@ private fun TripDetailScreenContent(
             )
         }
 
-        items(items = uiState.places, key = { it.timeLine }) { place ->
+        items(items = uiState.places, key = { "${it.id}${it.timeLine}" }) { place ->
             PlaceItem(
                 placeModel = place,
                 onTimeLineClick = onTimeLineClick,
