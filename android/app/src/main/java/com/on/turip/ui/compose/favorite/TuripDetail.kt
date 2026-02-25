dd2c519465a7689de550265d85752a168d94c691
jerry8282 <ka06196@naver.com>
2026-02-25 10:40:19 +0900

    refactor: ktlint적용

--- android/app/src/main/java/com/on/turip/ui/compose/favorite/TuripDetail.kt
+++ android/app/src/main/java/com/on/turip/ui/compose/favorite/TuripDetail.kt
@@ -157,8 +157,7 @@ private fun TuripPlaces(
                                 shadowElevation = elevation
                                 shape = reorderableItemShape
                                 clip = true
-                            }
-                            .draggableAfterLongPress(
+                            }.draggableAfterLongPress(
                                 interactionSource = interactionSource,
                             ),
                 )
