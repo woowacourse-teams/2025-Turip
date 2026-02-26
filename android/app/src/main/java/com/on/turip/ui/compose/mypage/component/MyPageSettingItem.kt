839a9b92c0e04ab13d9172519513b2ab5f7de503
yrsel <xxknu88@gmail.com>
2026-02-26 15:41:25 +0900

    refactor: 회원탈퇴 아이템 색상 수정 및 Preview 추가

--- android/app/src/main/java/com/on/turip/ui/compose/mypage/component/MyPageSettingItem.kt
+++ android/app/src/main/java/com/on/turip/ui/compose/mypage/component/MyPageSettingItem.kt
@@ -5,6 +5,7 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -15,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -29,6 +31,7 @@ fun MyPageSettingItem(
     @StringRes titleRes: Int,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
+    contentColor: Color = TuripTheme.colors.gray04,
 ) {
     val shape = TuripTheme.shape.container
 
@@ -51,7 +54,7 @@ fun MyPageSettingItem(
     ) {
         Icon(
             painter = painterResource(imageRes),
-            tint = TuripTheme.colors.gray03,
+            tint = contentColor,
             contentDescription = null,
             modifier =
                 Modifier
@@ -61,7 +64,7 @@ fun MyPageSettingItem(
         Text(
             text = stringResource(titleRes),
             style = TuripTheme.typography.info1,
-            color = TuripTheme.colors.gray04,
+            color = contentColor,
             textAlign = TextAlign.Left,
             modifier =
                 Modifier
@@ -80,11 +83,21 @@ fun MyPageSettingItem(
 @Composable
 private fun MyPageSettingItemPreview() {
     TuripTheme {
-        MyPageSettingItem(
-            imageRes = R.drawable.ic_inquire,
-            titleRes = R.string.my_page_inquiry,
-            onClick = {},
-            modifier = Modifier.padding(TuripTheme.spacing.small),
-        )
+        Column {
+            MyPageSettingItem(
+                imageRes = R.drawable.ic_inquire,
+                titleRes = R.string.my_page_inquiry,
+                onClick = {},
+                modifier = Modifier.padding(TuripTheme.spacing.small),
+            )
+
+            MyPageSettingItem(
+                imageRes = R.drawable.ic_inquire,
+                titleRes = R.string.my_page_inquiry,
+                onClick = {},
+                modifier = Modifier.padding(TuripTheme.spacing.small),
+                contentColor = TuripTheme.colors.error,
+            )
+        }
     }
 }
