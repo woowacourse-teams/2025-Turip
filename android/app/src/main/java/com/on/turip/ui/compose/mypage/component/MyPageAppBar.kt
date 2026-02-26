f3d580b65d855e2f0e7070317f8c964158489bc4
yrsel <xxknu88@gmail.com>
2026-02-26 17:25:00 +0900

    refactor: MyPageAppBar 타이틀 문자열 추출

--- android/app/src/main/java/com/on/turip/ui/compose/mypage/component/MyPageAppBar.kt
+++ android/app/src/main/java/com/on/turip/ui/compose/mypage/component/MyPageAppBar.kt
@@ -2,8 +2,9 @@ package com.on.turip.ui.compose.mypage.component
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.on.turip.R
 import com.on.turip.ui.compose.designsystem.component.TuripAppBar
 import com.on.turip.ui.compose.designsystem.theme.TuripTheme
 
@@ -12,8 +13,7 @@ fun MyPageAppBar() {
     TuripAppBar(
         start = {
             Text(
-                text = "마이페이지",
-                textAlign = TextAlign.Center,
+                text = stringResource(R.string.my_page_title),
                 style = TuripTheme.typography.display,
             )
         },
