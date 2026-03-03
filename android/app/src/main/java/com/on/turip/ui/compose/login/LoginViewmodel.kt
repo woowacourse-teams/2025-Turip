8d55eb22b1d582f60458298a0453b8fc98b0cd01
yrsel <xxknu88@gmail.com>
2026-03-03 17:45:50 +0900

    refactor: LoginViewModel 멤버, 토큰 로직 적용 및 수정

--- android/app/src/main/java/com/on/turip/ui/compose/login/LoginViewmodel.kt
+++ android/app/src/main/java/com/on/turip/ui/compose/login/LoginViewmodel.kt
@@ -2,16 +2,14 @@ package com.on.turip.ui.compose.login
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
-import com.on.turip.common.AuthState
-import com.on.turip.common.UserType
 import com.on.turip.core.result.ErrorType
 import com.on.turip.core.result.onFailure
 import com.on.turip.core.result.onSuccess
 import com.on.turip.data.login.datasource.GoogleCredentialManager
 import com.on.turip.domain.login.GuestRepository
 import com.on.turip.domain.login.MemberRepository
-import com.on.turip.domain.login.usecase.LoginUserUseCase
+import com.on.turip.domain.login.usecase.LoginUseCase
+import com.on.turip.domain.session.usecase.MoveToGuestUseCase
 import com.on.turip.ui.common.error.ErrorUiState
 import com.on.turip.ui.common.error.UiError
 import com.on.turip.ui.common.error.toUiError
@@ -28,9 +26,10 @@ import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewmodel @Inject constructor(
-    private val loginUserUseCase: LoginUserUseCase,
+    private val loginUseCase: LoginUseCase,
     private val memberRepository: MemberRepository,
     private val guestRepository: GuestRepository,
+    private val moveToGuestUseCase: MoveToGuestUseCase,
 ) : ViewModel() {
     private val _uiState: MutableStateFlow<LoginUiState> = MutableStateFlow(LoginUiState.IDLE)
     val uiState: StateFlow<LoginUiState> = _uiState
@@ -42,54 +41,34 @@ class LoginViewmodel @Inject constructor(
         _uiState.update { it.copy(showHelpText = show) }
     }
 
-    fun onGoogleLogin(googleCredentialManager: GoogleCredentialManager) {
+    fun loginWithGoogle(googleCredentialManager: GoogleCredentialManager) {
         viewModelScope.launch {
             googleCredentialManager
                 .getIdToken()
-                .onSuccess { result: GoogleIdTokenCredential ->
-                    loginUserUseCase(result.idToken)
+                .onSuccess { idToken: String ->
+                    loginUseCase(idToken)
                         .onSuccess { isNewMember: Boolean ->
-                            AuthState.change(UserType.MEMBER)
                             if (isNewMember) {
-                                showMigrationDialog(true)
+                                _uiState.update { it.copy(showMigrationDialog = true) }
                             } else {
                                 _uiEffect.send(LoginUiEffect.NavigateToMain)
                             }
-                        }.onFailure {
-                            Timber.e("Token 저장 실패")
-                            _uiEffect.send(LoginUiEffect.ShowError(ErrorUiState.Server))
+                        }.onFailure { errorType ->
+                            handleError(errorType)
                         }
                 }.onFailure { errorType: ErrorType ->
-                    val uiError: UiError = errorType.toUiError()
-                    if (uiError is UiError.Global) {
-                        when (uiError) {
-                            UiError.Global.Network -> {
-                                _uiEffect.send(LoginUiEffect.ShowError(ErrorUiState.Network))
-                            }
-
-                            UiError.Global.Server -> {
-                                _uiEffect.send(LoginUiEffect.ShowError(ErrorUiState.Server))
-                            }
-
-                            UiError.Global.TokenExpired -> {
-                                Unit
-                            }
-                        }
-                    }
-                    Timber.e("IdToken불러오기 실패")
+                    handleError(errorType)
+                    Timber.e("googleCredentialManager 에서 IdToken 불러오기 실패")
                 }
         }
     }
 
-    private fun showMigrationDialog(show: Boolean) {
-        _uiState.update { it.copy(showMigrationDialog = show) }
-    }
-
-    fun migration() {
+    fun confirmMigration() {
         viewModelScope.launch {
             memberRepository
                 .updateMigration()
                 .onSuccess {
+                    _uiState.update { it.copy(showMigrationDialog = false) }
                     _uiEffect.send(LoginUiEffect.NavigateToMain)
                 }.onFailure {
                     Timber.e("마이그레이션 실패")
@@ -109,10 +88,27 @@ class LoginViewmodel @Inject constructor(
         }
     }
 
-    fun onGuestLogin() {
+    fun continueAsGuest() {
         viewModelScope.launch {
-            AuthState.change(UserType.GUEST)
+            moveToGuestUseCase()
             _uiEffect.send(LoginUiEffect.NavigateToMain)
+        }
+    }
+
+    private suspend fun handleError(errorType: ErrorType) {
+        val uiError = errorType.toUiError()
+        when (uiError) {
+            UiError.Global.Network -> {
+                _uiEffect.send(LoginUiEffect.ShowError(ErrorUiState.Network))
+            }
+
+            UiError.Global.Server -> {
+                _uiEffect.send(LoginUiEffect.ShowError(ErrorUiState.Server))
+            }
+
+            else -> {
+                _uiEffect.send(LoginUiEffect.ShowError(ErrorUiState.Unexpected))
+            }
         }
     }
 }
