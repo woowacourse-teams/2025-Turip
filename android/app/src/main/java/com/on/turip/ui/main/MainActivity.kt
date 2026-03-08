f144d037c9494902f7f056185a28fd5cfb36cf5b
jerry8282 <ka06196@naver.com>
2026-03-08 09:33:05 +0900

    feat: 기기 크기에 따른 화면 방향 고정 로직 개선 및 불필요한 Composable 제거

--- android/app/src/main/java/com/on/turip/ui/main/MainActivity.kt
+++ android/app/src/main/java/com/on/turip/ui/main/MainActivity.kt
@@ -7,9 +7,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.platform.LocalConfiguration
 import com.on.turip.ui.compose.main.component.MainApp
 import com.on.turip.ui.compose.main.navigation.SavedStateConfigurationProvider
 import dagger.hilt.android.AndroidEntryPoint
@@ -24,17 +21,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
-            val isPhone = rememberIsPhone()
+            val shouldLockPortrait = resources.configuration.smallestScreenWidthDp < 600
             requestedOrientation =
-                if (isPhone) ActivityInfo.SCREEN_ORIENTATION_PORTRAIT else ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+                if (shouldLockPortrait) ActivityInfo.SCREEN_ORIENTATION_PORTRAIT else ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
             MainApp(savedStateConfigurationProvider)
         }
-    }
-
-    @Composable
-    private fun rememberIsPhone(): Boolean {
-        val configuration = LocalConfiguration.current
-        return remember { configuration.smallestScreenWidthDp < 600 }
     }
 
     companion object {
