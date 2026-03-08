5848a4f271d4779bfe57d5952fefb3289dcf9457
jerry8282 <ka06196@naver.com>
2026-03-08 09:18:25 +0900

    refactor: NavigationState 키 조회 시 예외 처리 추가

--- android/app/src/main/java/com/on/turip/navigation/NavigationState.kt
+++ android/app/src/main/java/com/on/turip/navigation/NavigationState.kt
@@ -55,8 +55,12 @@ class NavigationState(
 ) {
     val isTopLevelKey
         get() = topLevelKeys.contains(currentKey)
-    val currentTopLevelKey: NavKey by derivedStateOf { topLevelStack.last() }
-    val currentKey: NavKey by derivedStateOf { currentSubStack.last() }
+    val currentTopLevelKey: NavKey by derivedStateOf {
+        topLevelStack.lastOrNull() ?: throw IllegalStateException("등록된 Top키가 존재하지 않습니다.")
+    }
+    val currentKey: NavKey by derivedStateOf {
+        currentSubStack.lastOrNull() ?: throw IllegalStateException("등록된 키가 존재하지 않습니다.")
+    }
 
     val topLevelKeys
         get() = subStacks.keys
