c96aae03a9cc126ac07cb1aec36556b18ea25691
jerry8282 <ka06196@naver.com>
2026-03-11 01:09:12 +0900

    refactor: TuripSnackbar 내 imePadding 제거

--- android/app/src/main/java/com/on/turip/ui/compose/designsystem/component/TuripSnackbar.kt
+++ android/app/src/main/java/com/on/turip/ui/compose/designsystem/component/TuripSnackbar.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Snackbar
@@ -37,7 +36,7 @@ fun TuripSnackbar(
     snackbarHostState: SnackbarHostState,
     modifier: Modifier = Modifier,
 ) {
-    Box(modifier = modifier.imePadding()) {
+    Box(modifier = modifier) {
         SnackbarHost(
             hostState = snackbarHostState,
             snackbar = { snackbarData: SnackbarData ->
