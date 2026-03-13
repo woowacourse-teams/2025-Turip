58a5bb22c0629b0067acbf5961168445e005fd4f
yrsel <xxknu88@gmail.com>
2026-03-13 16:36:39 +0900

    refactor: 토큰을 url로 변환하는 로직 인코딩 로직 추가

--- android/app/src/main/java/com/on/turip/ui/common/extensions/TuripLinkExtensions.kt
+++ android/app/src/main/java/com/on/turip/ui/common/extensions/TuripLinkExtensions.kt
@@ -2,5 +2,14 @@ package com.on.turip.ui.common.extensions
 
 import com.on.turip.BuildConfig
 import com.on.turip.domain.turip.TuripInvitationToken
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 
-fun TuripInvitationToken.toUrl(): String = "https://${BuildConfig.APP_LINK_TURIP_INVITATION_HOST}?token=$value"
+/**
+ * 초대 토큰을 앱 링크 URL로 변환한다.
+ * query string 손상을 막기 위해 토큰 값은 UTF-8로 URL 인코딩한다.
+ */
+fun TuripInvitationToken.toUrl(): String {
+    val encodedToken = URLEncoder.encode(value, StandardCharsets.UTF_8.name())
+    return "https://${BuildConfig.APP_LINK_TURIP_INVITATION_HOST}?token=$encodedToken"
+}
