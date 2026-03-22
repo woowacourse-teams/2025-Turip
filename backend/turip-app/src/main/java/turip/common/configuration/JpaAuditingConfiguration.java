376bfe03a8897cc5c38bed394943be1c3c935b81
시원 <cnc4750@inha.edu>
2026-03-22 16:46:02 +0900

    feat: ContentPending 테이블에 contentDataUpdatedAt 필드 추가

---
+++ backend/turip-app/src/main/java/turip/common/configuration/JpaAuditingConfiguration.java
@@ -0,0 +1,9 @@
+package turip.common.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+}
