43b7afc58b8a109b2b331f5f1da0a67d16e19396
jerry8282 <ka06196@naver.com>
2026-03-31 07:05:14 +0900

    feat: BookmarkRemoteDataSource 내 postBookmark 반환 타입을 BookmarkCreationResponse로 변경

--- android/app/src/main/java/com/on/turip/data/bookmark/datasource/DefaultBookmarkRemoteDataSource.kt
+++ android/app/src/main/java/com/on/turip/data/bookmark/datasource/DefaultBookmarkRemoteDataSource.kt
@@ -5,6 +5,7 @@ import com.on.turip.core.result.mapCatching
 import com.on.turip.data.bookmark.dto.BookmarkAddRequest
 import com.on.turip.data.bookmark.dto.BookmarkContentsResponse
 import com.on.turip.data.bookmark.dto.BookmarkCountResponse
+import com.on.turip.data.bookmark.dto.BookmarkCreationResponse
 import com.on.turip.data.bookmark.service.BookmarkService
 import com.on.turip.data.result.safeApiCall
 import com.on.turip.domain.common.paging.Cursor
@@ -17,9 +18,9 @@ class DefaultBookmarkRemoteDataSource @Inject constructor(
     private val bookmarkService: BookmarkService,
     private val coroutineContext: CoroutineContext = Dispatchers.IO,
 ) : BookmarkRemoteDataSource {
-    override suspend fun postBookmark(bookmarkAddRequest: BookmarkAddRequest): TuripResult<Unit> =
+    override suspend fun postBookmark(bookmarkAddRequest: BookmarkAddRequest): TuripResult<BookmarkCreationResponse> =
         withContext(coroutineContext) {
-            safeApiCall { bookmarkService.postBookmark(bookmarkAddRequest) }.mapCatching { Unit }
+            safeApiCall { bookmarkService.postBookmark(bookmarkAddRequest) }
         }
 
     override suspend fun deleteBookmark(contentId: Long): TuripResult<Unit> =
