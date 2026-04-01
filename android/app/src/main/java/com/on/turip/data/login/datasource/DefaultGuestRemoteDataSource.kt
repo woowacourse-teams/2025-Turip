8dc9d63fa00687ae68d78377e549f60d475f27d5
yrsel <xxknu88@gmail.com>
2026-04-01 13:51:37 +0900

    refactor: CoroutineContext 주입 받도록해서 테스트 가능한 구조로 수정

--- android/app/src/main/java/com/on/turip/data/login/datasource/DefaultGuestRemoteDataSource.kt
+++ android/app/src/main/java/com/on/turip/data/login/datasource/DefaultGuestRemoteDataSource.kt
@@ -3,10 +3,17 @@ package com.on.turip.data.login.datasource
 import com.on.turip.core.result.TuripResult
 import com.on.turip.data.login.service.GuestService
 import com.on.turip.data.result.safeApiCall
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
 
 class DefaultGuestRemoteDataSource @Inject constructor(
     private val guestService: GuestService,
+    private val coroutineContext: CoroutineContext = Dispatchers.IO,
 ) : GuestRemoteDataSource {
-    override suspend fun deleteGuest(): TuripResult<Unit> = safeApiCall { guestService.deleteGuest() }
+    override suspend fun deleteGuest(): TuripResult<Unit> =
+        withContext(coroutineContext) {
+            safeApiCall { guestService.deleteGuest() }
+        }
 }
--- android/app/src/main/java/com/on/turip/data/login/datasource/DefaultMemberRemoteDataSource.kt
+++ android/app/src/main/java/com/on/turip/data/login/datasource/DefaultMemberRemoteDataSource.kt
@@ -3,14 +3,27 @@ package com.on.turip.data.login.datasource
 import com.on.turip.core.result.TuripResult
 import com.on.turip.data.login.service.MemberService
 import com.on.turip.data.result.safeApiCall
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
 
 class DefaultMemberRemoteDataSource @Inject constructor(
     private val memberService: MemberService,
+    private val coroutineContext: CoroutineContext = Dispatchers.IO,
 ) : MemberRemoteDataSource {
-    override suspend fun postMigration(): TuripResult<Unit> = safeApiCall { memberService.postMigration() }
+    override suspend fun postMigration(): TuripResult<Unit> =
+        withContext(coroutineContext) {
+            safeApiCall { memberService.postMigration() }
+        }
 
-    override suspend fun postLogout(): TuripResult<Unit> = safeApiCall { memberService.postLogout() }
+    override suspend fun postLogout(): TuripResult<Unit> =
+        withContext(coroutineContext) {
+            safeApiCall { memberService.postLogout() }
+        }
 
-    override suspend fun deleteMember(): TuripResult<Unit> = safeApiCall { memberService.deleteMember() }
+    override suspend fun deleteMember(): TuripResult<Unit> =
+        withContext(coroutineContext) {
+            safeApiCall { memberService.deleteMember() }
+        }
 }
