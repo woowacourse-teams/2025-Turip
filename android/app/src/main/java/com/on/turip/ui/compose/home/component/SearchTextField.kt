8d742d41e1623c8175eb4612df5989a768d28213
jerry8282 <ka06196@naver.com>
2026-04-08 11:46:44 +0900

    refactor: SearchTextField 내 검색 아이콘을 Material 기본 아이콘으로 교체 및 미사용 리소스 삭제

--- android/app/src/main/java/com/on/turip/ui/compose/home/component/SearchTextField.kt
+++ android/app/src/main/java/com/on/turip/ui/compose/home/component/SearchTextField.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
@@ -15,7 +17,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
@@ -65,7 +66,7 @@ fun SearchTextField(
                 if (trimmedKeyword.isNotBlank()) onSearch(trimmedKeyword)
             }) {
                 Icon(
-                    painter = painterResource(R.drawable.btn_search),
+                    imageVector = Icons.Default.Search,
                     contentDescription = null,
                     tint = TuripTheme.colors.gray03,
                 )
