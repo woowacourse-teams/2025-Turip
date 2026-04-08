06ac1200e7e93197db7636c5c2bf7bef81876629
jerry8282 <ka06196@naver.com>
2026-04-08 14:12:27 +0900

    refactor: ktlint적용

--- android/app/src/main/java/com/on/turip/ui/compose/trip/turipselection/component/ShareOptionBottomSheet.kt
+++ android/app/src/main/java/com/on/turip/ui/compose/trip/turipselection/component/ShareOptionBottomSheet.kt
@@ -1,7 +1,6 @@
 package com.on.turip.ui.compose.trip.turipselection.component
 
 import androidx.activity.compose.BackHandler
-import androidx.annotation.DrawableRes
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -26,7 +25,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
