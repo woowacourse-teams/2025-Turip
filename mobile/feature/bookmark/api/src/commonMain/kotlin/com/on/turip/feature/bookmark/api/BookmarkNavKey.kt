97d1376dd53b39465e7d4580ad4963f6c0732fc3
jerry8282 <ka06196@naver.com>
2026-05-28 23:31:19 +0900

    [feat] 각 기능(feature) 모듈별 `NavKey` 정의 추가

---
+++ mobile/feature/bookmark/api/src/commonMain/kotlin/com/on/turip/feature/bookmark/api/BookmarkNavKey.kt
@@ -0,0 +1,7 @@
+package com.on.turip.feature.bookmark.api
+
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object BookmarkNavKey : NavKey
---
+++ mobile/feature/turip/api/src/commonMain/kotlin/com/on/turip/feature/turip/api/MyTuripNavKey.kt
@@ -0,0 +1,7 @@
+package com.on.turip.feature.turip.api
+
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object MyTuripNavKey : NavKey
