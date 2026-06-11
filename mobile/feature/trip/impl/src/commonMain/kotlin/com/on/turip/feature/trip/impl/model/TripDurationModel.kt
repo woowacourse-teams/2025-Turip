891a0fa2b778548ae022d9520920138e415286e2
jerry8282 <ka06196@naver.com>
2026-06-12 00:55:02 +0900

    [feat] `TripDurationModel` 내 하드코딩된 문자열 리소스화 및 형식 지정자 수정

--- mobile/feature/trip/impl/src/commonMain/kotlin/com/on/turip/feature/trip/impl/model/TripDurationModel.kt
+++ mobile/feature/trip/impl/src/commonMain/kotlin/com/on/turip/feature/trip/impl/model/TripDurationModel.kt
@@ -1,13 +1,22 @@
 package com.on.turip.feature.trip.impl.model
 
+import androidx.compose.runtime.Composable
+import com.on.turip.core.designsystem.generated.resources.Res
+import com.on.turip.core.designsystem.generated.resources.all_trip_day_trip_duration
+import com.on.turip.core.designsystem.generated.resources.all_trip_duration
+import org.jetbrains.compose.resources.stringResource
+
 data class TripDurationModel(
     val nights: Int,
     val days: Int,
 ) {
+    private val isDayTrip: Boolean = nights == 0 && days == 1
+
+    @Composable
     fun toDisplayText(): String =
-        if (nights == 0 && days == 1) {
-            "당일치기"
+        if (isDayTrip) {
+            stringResource(Res.string.all_trip_day_trip_duration)
         } else {
-            "${nights}박 ${days}일"
+            stringResource(Res.string.all_trip_duration, nights, days)
         }
 }
