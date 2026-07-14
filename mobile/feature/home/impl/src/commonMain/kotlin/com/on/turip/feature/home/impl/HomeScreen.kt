5505bb8688ea52284f4599d4786a7be7c19b435f
jerry8282 <ka06196@naver.com>
2026-07-14 21:45:23 +0900

    refactor: `HomeScreen`의 패딩 설정을 `systemBarsPadding`에서 `statusBarsPadding`으로 변경

--- mobile/feature/home/impl/src/commonMain/kotlin/com/on/turip/feature/home/impl/HomeScreen.kt
+++ mobile/feature/home/impl/src/commonMain/kotlin/com/on/turip/feature/home/impl/HomeScreen.kt
@@ -10,7 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -71,7 +71,7 @@ fun HomeScreen(
             modifier =
                 Modifier
                     .fillMaxWidth()
-                    .systemBarsPadding()
+                    .statusBarsPadding()
                     .background(TuripTheme.colors.white),
         )
         HomeAppBar()
