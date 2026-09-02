430910957e0e85df1a8185bfb7276daf47fd640b
시원 <cnc4750@inha.edu>
2026-09-03 00:53:52 +0900

    feat: WebClient 디폴트 Configuration 추가

---
+++ backend/turip-app/src/main/java/turip/common/configuration/WebClientConfiguration.java
@@ -0,0 +1,35 @@
+package turip.common.configuration;
+
+import io.netty.channel.ChannelOption;
+import java.time.Duration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
+
+@Configuration
+public class WebClientConfiguration {
+
+    private static final int MAX_CONNECTIONS = 10;
+    private static final int CONNECT_TIMEOUT_MILLIS = 2000;
+    private static final Duration PENDING_ACQUIRE_TIMEOUT = Duration.ofSeconds(3);
+    private static final Duration RESPONSE_TIMEOUT = Duration.ofSeconds(5);
+
+    @Bean
+    public WebClient baseWebClient() {
+        ConnectionProvider connectionProvider = ConnectionProvider.builder("base-webclient")
+                .maxConnections(MAX_CONNECTIONS)
+                .pendingAcquireTimeout(PENDING_ACQUIRE_TIMEOUT)
+                .build();
+
+        HttpClient httpClient = HttpClient.create(connectionProvider)
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, CONNECT_TIMEOUT_MILLIS)
+                .responseTimeout(RESPONSE_TIMEOUT);
+
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
+    }
+}
